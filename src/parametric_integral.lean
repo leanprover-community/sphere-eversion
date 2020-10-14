f2570bf8ceb9138e89cf670805a714ecaa932f80
Patrick Massot <patrickmassot@free.fr>
2020-10-14 18:28:30 +0200

    More parametric integral

--- src/parametric_integral.lean
+++ src/parametric_integral.lean
@@ -33,6 +33,17 @@ end
 
 end
 
+section loc_integrable
+
+variables {α : Type*} [measurable_space α] [topological_space α]
+
+variables {E : Type*} [normed_group E] [second_countable_topology E] [normed_space ℝ E]
+  [complete_space E] [measurable_space E] [borel_space E]
+
+def loc_integrable (f : α → E) (μ : measure α . volume_tac) := ∀ K, is_compact K → integrable_on f K μ
+
+end loc_integrable
+
 section interval_integrable
 open set
 
@@ -40,27 +51,36 @@ variables {α : Type*} [decidable_linear_order α] {P : α → Prop} {a b : α}
 
 def interval_oc : α → α → set α := λ a b, Ioc (min a b) (max a b)
 
-lemma forall_interval_oc_iff : 
+lemma interval_oc_of_le (h : a ≤ b) : interval_oc a b = Ioc a b :=
+by simp [interval_oc, h]
+
+lemma interval_oc_of_gt (h : a > b) : interval_oc a b = Ioc b a :=
+by simp [interval_oc, le_of_lt h]
+
+lemma forall_interval_oc_iff :
   (∀ x ∈ interval_oc a b, P x) ↔ (∀ x ∈ Ioc a b, P x) ∧ (∀ x ∈ Ioc b a, P x) :=
 by { dsimp [interval_oc], cases le_total a b with hab hab ; simp [hab] }
 
-variables {E : Type*} [measurable_space α] {μ : measure α} [normed_group E] 
+variables {E : Type*} [measurable_space α] {μ : measure α} [normed_group E]
 
-lemma ae_interval_oc__iff : 
+lemma ae_interval_oc_iff :
   (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔ (∀ᵐ x ∂μ, x ∈ Ioc a b → P x) ∧ (∀ᵐ x ∂μ, x ∈ Ioc b a → P x) :=
 by { dsimp [interval_oc], cases le_total a b with hab hab ; simp [hab] }
 
 variables  [topological_space α] [opens_measurable_space α] [order_closed_topology α]
 
-lemma ae_interval_oc__iff' : (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔ 
+lemma ae_interval_oc_iff' : (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔
   (∀ᵐ x ∂ (μ.restrict $ Ioc a b), P x) ∧ (∀ᵐ x ∂ (μ.restrict $ Ioc b a), P x) :=
 begin
-  simp_rw ae_interval_oc__iff,
-  rw [ae_restrict_eq, eventually_inf_principal, ae_restrict_eq, eventually_inf_principal] ;   
+  simp_rw ae_interval_oc_iff,
+  rw [ae_restrict_eq, eventually_inf_principal, ae_restrict_eq, eventually_inf_principal] ;
   exact is_measurable_Ioc
 end
 
 end interval_integrable
+
+-- Below is a capital iota
+local notation `Ι` := interval_oc
 
 variables {α : Type*} [measurable_space α] {μ : measure α}
 
@@ -81,12 +101,61 @@ lemma continuous_at_of_dominated {F : X → α → E} {x₀ : X} {bound : α →
 tendsto_integral_filter_of_dominated_convergence bound
   (first_countable_topology.nhds_generated_countable x₀) ‹_› (mem_of_nhds hF_meas) ‹_› ‹_› ‹_›
 
-lemma continuous_of_dominated {F : X → α → E} {x₀ : X} {bound : α → ℝ}
+lemma continuous_of_dominated {F : X → α → E} {bound : α → ℝ}
   (hF_meas : ∀ x, measurable (F x)) (h_bound : ∀ x, ∀ᵐ a ∂μ, ∥F x a∥ ≤ bound a)
   (bound_integrable : integrable bound μ) (h_cont : ∀ᵐ a ∂μ, continuous (λ x, F x a)) :
   continuous (λn, ∫ a, F n a ∂μ) :=
 continuous_iff_continuous_at.mpr (λ x₀, continuous_at_of_dominated (eventually_of_forall hF_meas)
   (eventually_of_forall h_bound) ‹_› $ h_cont.mono $ λ _, continuous.continuous_at)
+
+section interval
+variables [decidable_linear_order α]
+
+lemma continuous_at_of_dominated_interval {F : X → α → E} {x₀ : X} {bound : α → ℝ} {a b : α}
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (h_bound : ∀ᶠ x in 𝓝 x₀, ∀ᵐ t ∂(μ.restrict $ Ι a b), ∥F x t∥ ≤ bound t)
+  (bound_integrable : interval_integrable bound μ a b)
+  (h_cont : ∀ᵐ t ∂(μ.restrict $ Ι a b), continuous_at (λ x, F x t) x₀) :
+  continuous_at (λn, ∫ t in a..b, F n t ∂μ) x₀ :=
+begin
+  have gc := first_countable_topology.nhds_generated_countable x₀,
+  cases bound_integrable,
+  cases le_or_gt a b with hab hab;
+  [{ rw interval_oc_of_le hab at *,
+     simp_rw interval_integral.integral_of_le hab },
+   { rw interval_oc_of_gt hab at *,
+     simp_rw interval_integral.integral_of_ge (le_of_lt hab),
+     refine continuous_at.neg _ }];
+  apply tendsto_integral_filter_of_dominated_convergence bound gc hF_meas (mem_of_nhds hF_meas) h_bound,
+  exact bound_integrable_left,
+  exact h_cont,
+  exact bound_integrable_right,
+  exact h_cont
+end
+
+lemma continuous_of_dominated_interval {F : X → α → E} {bound : α → ℝ} {a b : α}
+  (hF_meas : ∀ x, measurable (F x))
+  (h_bound : ∀ x, ∀ᵐ t ∂(μ.restrict $ Ι a b), ∥F x t∥ ≤ bound t)
+  (bound_integrable : interval_integrable bound μ a b)
+  (h_cont : ∀ᵐ t ∂(μ.restrict $ Ι a b), continuous (λ x, F x t)) :
+  continuous (λn, ∫ t in a..b, F n t ∂μ) :=
+continuous_iff_continuous_at.mpr (λ x₀, continuous_at_of_dominated_interval (eventually_of_forall hF_meas)
+  (eventually_of_forall h_bound) ‹_› $ h_cont.mono $ λ _, continuous.continuous_at)
+
+/- The next lemma uses the Lebesgue measure on ℝ. -/
+
+lemma continuous_of_dominated_interval' {F : X → ℝ → E} {b : X → ℝ} {bound : ℝ → ℝ} {a : ℝ}
+  {x₀ : X}
+  (F_cont : ∀ᵐ t, continuous_at (λ x, F x t) x₀)
+  (b_cont : continuous_at b x₀)
+  (h_bound : ∀ᶠ x in 𝓝 x₀, ∀ t, ∥F x t∥ ≤ bound t)
+  (h : loc_integrable bound) :
+  continuous_at (λ x, ∫ t in a..b x, F x t) x₀ :=
+begin
+
+  sorry
+end
+end interval
 
 lemma integrable_of_norm_sub_le {f₀ f₁ : α → E} {g : α → ℝ}
   (hf₀_m : measurable f₀)
@@ -286,7 +355,7 @@ variables (ν : measure ℝ)
 local notation `I` := interval_oc
 
 lemma has_fderiv_at_of_dominated_of_fderiv_le'' {F : H → ℝ → E} {F' : H → ℝ → (H →L[ℝ] E)} {x₀ : H}
-  {a b : ℝ}  
+  {a b : ℝ}
   {bound : ℝ → ℝ}
   {ε : ℝ}
   (ε_pos : 0 < ε)
@@ -299,11 +368,11 @@ lemma has_fderiv_at_of_dominated_of_fderiv_le'' {F : H → ℝ → E} {F' : H �
   (h_diff : ∀ᵐ t ∂ν, t ∈ I a b → ∀ x ∈ ball x₀ ε, has_fderiv_at (λ x, F x t) (F' x t) x) :
   has_fderiv_at (λ x, ∫ t in a..b, F x t ∂ν) (∫ t in a..b, F' x₀ t ∂ν) x₀ :=
 begin
-  erw ae_interval_oc__iff' at h_diff h_bound,
+  erw ae_interval_oc_iff' at h_diff h_bound,
   exact has_fderiv_at.sub
-    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.1 hF'_meas 
+    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.1 hF'_meas
       h_bound.1 bound_measurable bound_integrable.1 h_diff.1)
-    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.2 hF'_meas 
+    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.2 hF'_meas
       h_bound.2 bound_measurable bound_integrable.2 h_diff.2)
 end
 
