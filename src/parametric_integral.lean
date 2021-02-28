7c93b17de84e858569bdea266fbd2354f5a09a89
Patrick Massot <patrickmassot@free.fr>
2021-02-28 15:58:16 +0100

    Bump mathlib
    Ajust to the big integration refactor of
    https://github.com/leanprover-community/mathlib/pull/5510

--- src/parametric_integral.lean
+++ src/parametric_integral.lean
@@ -23,9 +23,9 @@ variables (𝕜 : Type*) [nondiscrete_normed_field 𝕜]
           {E : Type*} [normed_group E] [normed_space 𝕜 E]
           (F : Type*) [normed_group F] [normed_space 𝕜 F]
 
-lemma continuous_linear_map.continuous_apply (v : E) : continuous (continuous_linear_map.applyₗ 𝕜 F v) :=
+lemma continuous_linear_map.continuous_apply (v : E) : continuous (continuous_linear_map.apply 𝕜 F v) :=
 begin
-  apply (continuous_linear_map.applyₗ 𝕜 F v).continuous_of_bound,
+  apply linear_map.continuous_of_bound,
   intro f,
   rw mul_comm,
   exact f.le_op_norm v,
@@ -37,9 +37,10 @@ section loc_integrable
 
 variables {α : Type*} [measurable_space α] [topological_space α]
 
-variables {E : Type*} [normed_group E] [second_countable_topology E] [normed_space ℝ E]
-  [complete_space E] [measurable_space E] [borel_space E]
+variables {E : Type*} [normed_group E] -- [second_countable_topology E] [normed_space ℝ E]
+  [measurable_space E] --[borel_space E]
 
+/-- A function is locally integrable if it is integrable on each compact set. -/
 def loc_integrable (f : α → E) (μ : measure α . volume_tac) := ∀ K, is_compact K → integrable_on f K μ
 
 end loc_integrable
@@ -47,14 +48,15 @@ end loc_integrable
 section interval_integrable
 open set
 
-variables {α : Type*} [decidable_linear_order α] {P : α → Prop} {a b : α}
+variables {α : Type*} [linear_order α] {P : α → Prop} {a b : α}
 
+/-- The open-closed interval with unordered bounds. -/
 def interval_oc : α → α → set α := λ a b, Ioc (min a b) (max a b)
 
 lemma interval_oc_of_le (h : a ≤ b) : interval_oc a b = Ioc a b :=
 by simp [interval_oc, h]
 
-lemma interval_oc_of_gt (h : a > b) : interval_oc a b = Ioc b a :=
+lemma interval_oc_of_lt (h : b < a) : interval_oc a b = Ioc b a :=
 by simp [interval_oc, le_of_lt h]
 
 lemma forall_interval_oc_iff :
@@ -67,6 +69,12 @@ lemma ae_interval_oc_iff :
   (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔ (∀ᵐ x ∂μ, x ∈ Ioc a b → P x) ∧ (∀ᵐ x ∂μ, x ∈ Ioc b a → P x) :=
 by { dsimp [interval_oc], cases le_total a b with hab hab ; simp [hab] }
 
+lemma ae_measurable_interval_oc_iff {μ : measure α} {β : Type*} [measurable_space β] {f : α → β} :
+  (ae_measurable f $ μ.restrict $ interval_oc a b) ↔
+  (ae_measurable f $ μ.restrict $ Ioc a b) ∧ (ae_measurable f $ μ.restrict $ Ioc b a) :=
+by { dsimp [interval_oc], cases le_total a b with hab hab ; simp [hab] }
+
+
 variables  [topological_space α] [opens_measurable_space α] [order_closed_topology α]
 
 lemma ae_interval_oc_iff' : (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔
@@ -74,7 +82,7 @@ lemma ae_interval_oc_iff' : (∀ᵐ x ∂μ, x ∈ interval_oc a b → P x) ↔
 begin
   simp_rw ae_interval_oc_iff,
   rw [ae_restrict_eq, eventually_inf_principal, ae_restrict_eq, eventually_inf_principal] ;
-  exact is_measurable_Ioc
+  exact measurable_set_Ioc,
 end
 
 end interval_integrable
@@ -95,24 +103,24 @@ variables {E : Type*} [normed_group E] [second_countable_topology E] [normed_spa
 variables {X : Type*} [topological_space X] [first_countable_topology X]
 
 lemma continuous_at_of_dominated {F : X → α → E} {x₀ : X} {bound : α → ℝ}
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x)) (h_bound : ∀ᶠ x in 𝓝 x₀, ∀ᵐ a ∂μ, ∥F x a∥ ≤ bound a)
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) μ) (h_bound : ∀ᶠ x in 𝓝 x₀, ∀ᵐ a ∂μ, ∥F x a∥ ≤ bound a)
   (bound_integrable : integrable bound μ) (h_cont : ∀ᵐ a ∂μ, continuous_at (λ x, F x a) x₀) :
   continuous_at (λn, ∫ a, F n a ∂μ) x₀ :=
 tendsto_integral_filter_of_dominated_convergence bound
-  (first_countable_topology.nhds_generated_countable x₀) ‹_› (mem_of_nhds hF_meas) ‹_› ‹_› ‹_›
+  (first_countable_topology.nhds_generated_countable x₀) ‹_› (mem_of_nhds hF_meas : _) ‹_› ‹_› ‹_›
 
 lemma continuous_of_dominated {F : X → α → E} {bound : α → ℝ}
-  (hF_meas : ∀ x, measurable (F x)) (h_bound : ∀ x, ∀ᵐ a ∂μ, ∥F x a∥ ≤ bound a)
+  (hF_meas : ∀ x, ae_measurable (F x) μ) (h_bound : ∀ x, ∀ᵐ a ∂μ, ∥F x a∥ ≤ bound a)
   (bound_integrable : integrable bound μ) (h_cont : ∀ᵐ a ∂μ, continuous (λ x, F x a)) :
   continuous (λn, ∫ a, F n a ∂μ) :=
 continuous_iff_continuous_at.mpr (λ x₀, continuous_at_of_dominated (eventually_of_forall hF_meas)
   (eventually_of_forall h_bound) ‹_› $ h_cont.mono $ λ _, continuous.continuous_at)
 
 section interval
-variables [decidable_linear_order α]
+variables [linear_order α]
 
 lemma continuous_at_of_dominated_interval {F : X → α → E} {x₀ : X} {bound : α → ℝ} {a b : α}
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) (μ.restrict $ Ι a b))
   (h_bound : ∀ᶠ x in 𝓝 x₀, ∀ᵐ t ∂(μ.restrict $ Ι a b), ∥F x t∥ ≤ bound t)
   (bound_integrable : interval_integrable bound μ a b)
   (h_cont : ∀ᵐ t ∂(μ.restrict $ Ι a b), continuous_at (λ x, F x t) x₀) :
@@ -123,10 +131,10 @@ begin
   cases le_or_gt a b with hab hab;
   [{ rw interval_oc_of_le hab at *,
      simp_rw interval_integral.integral_of_le hab },
-   { rw interval_oc_of_gt hab at *,
+   { rw interval_oc_of_lt hab at *,
      simp_rw interval_integral.integral_of_ge (le_of_lt hab),
-     refine continuous_at.neg _ }];
-  apply tendsto_integral_filter_of_dominated_convergence bound gc hF_meas (mem_of_nhds hF_meas) h_bound,
+     refine tendsto.neg _ }];
+  apply tendsto_integral_filter_of_dominated_convergence bound gc hF_meas (mem_of_nhds hF_meas : _) h_bound,
   exact bound_integrable_left,
   exact h_cont,
   exact bound_integrable_right,
@@ -134,7 +142,7 @@ begin
 end
 
 lemma continuous_of_dominated_interval {F : X → α → E} {bound : α → ℝ} {a b : α}
-  (hF_meas : ∀ x, measurable (F x))
+  (hF_meas : ∀ x, ae_measurable (F x) $ μ.restrict $ Ι a b)
   (h_bound : ∀ x, ∀ᵐ t ∂(μ.restrict $ Ι a b), ∥F x t∥ ≤ bound t)
   (bound_integrable : interval_integrable bound μ a b)
   (h_cont : ∀ᵐ t ∂(μ.restrict $ Ι a b), continuous (λ x, F x t)) :
@@ -157,10 +165,13 @@ begin
 end
 end interval
 
+end
+
+variables {E : Type*} [normed_group E] [measurable_space E] [borel_space E]
+
 lemma integrable_of_norm_sub_le {f₀ f₁ : α → E} {g : α → ℝ}
-  (hf₀_m : measurable f₀)
+  (hf₁_m : ae_measurable f₁ μ)
   (hf₀_i : integrable f₀ μ)
-  (hg_m : measurable g)
   (hg_i : integrable g μ)
   (h : ∀ᵐ a ∂μ, ∥f₀ a - f₁ a∥ ≤ g a) :
   integrable f₁ μ :=
@@ -170,10 +181,13 @@ begin
     intros a ha,
     calc ∥f₁ a∥ ≤ ∥f₀ a∥ + ∥f₀ a - f₁ a∥ : norm_le_insert _ _
     ... ≤ ∥f₀ a∥ + g a : add_le_add_left ha _ },
-  exact integrable.mono' (hf₀_i.norm.add  hf₀_m.norm hg_m hg_i) this,
+  exact integrable.mono' (hf₀_i.norm.add hg_i) hf₁_m this
 end
 
-variables {H : Type*} [normed_group H] [normed_space ℝ H] [measurable_space H]
+section
+variables
+   [normed_space ℝ E]
+  {H : Type*} [normed_group H] [normed_space ℝ H] [measurable_space H]
   [second_countable_topology $ H →L[ℝ] E] [measurable_space $ H →L[ℝ] E]
   [borel_space $ H →L[ℝ] E]
 
@@ -181,31 +195,36 @@ lemma measurable.apply_continuous_linear_map {φ : α → H →L[ℝ] E} (hφ : 
   measurable (λ a, φ a v) :=
 (continuous_linear_map.continuous_apply _ _ v).measurable.comp hφ
 
-lemma measure_theory.integrable.apply_continuous_linear_map {φ : α → H →L[ℝ] E}
-  (φ_meas : measurable φ) (φ_int : integrable φ μ) (v : H) : integrable (λ a, φ a v) μ :=
-begin
-  apply (φ_int.norm.mul_const _).mono',
-  apply eventually_of_forall,
-  intro a,
-  exact (φ a).le_op_norm v,
+lemma ae_measurable.apply_continuous_linear_map {φ : α → H →L[ℝ] E} (hφ : ae_measurable φ μ) (v : H) :
+  ae_measurable (λ a, φ a v) μ :=
+(continuous_linear_map.continuous_apply _ _ v).measurable.comp_ae_measurable hφ
 end
 
-lemma continuous_linear_map.apply_integral {φ : α → H →L[ℝ] E} (φ_meas : measurable φ)
+variables [second_countable_topology E] [normed_space ℝ E] [complete_space E]
+  {H : Type*} [normed_group H] [normed_space ℝ H] [measurable_space H]
+  [second_countable_topology $ H →L[ℝ] E] [measurable_space $ H →L[ℝ] E]
+  [borel_space $ H →L[ℝ] E]
+
+lemma measure_theory.integrable.apply_continuous_linear_map {φ : α → H →L[ℝ] E}
+  (φ_int : integrable φ μ) (v : H) : integrable (λ a, φ a v) μ :=
+(φ_int.norm.mul_const ∥v∥).mono' (φ_int.ae_measurable.apply_continuous_linear_map v)
+  (eventually_of_forall $ λ a, (φ a).le_op_norm v)
+
+lemma continuous_linear_map.apply_integral {φ : α → H →L[ℝ] E}
   (φ_int : integrable φ μ) (v : H) : ∫ a, φ a v ∂μ = (∫ a, φ a ∂μ) v :=
-(continuous_linear_map.apply ℝ E v).integral_comp_comm φ_meas φ_int
+(continuous_linear_map.apply ℝ E v).integral_comp_comm φ_int
 
 lemma measurable_abs : measurable (abs : ℝ → ℝ) :=
-real.continuous_abs.measurable
+continuous_abs.measurable
 
 lemma has_fderiv_at_of_dominated_loc_of_lip' {F : H → α → E} {F' : α → (H →L[ℝ] E)} {x₀ : H}
   {bound : α → ℝ}
   {ε : ℝ}
   (ε_pos : 0 < ε)
-  (hF_meas : ∀ x ∈ ball x₀ ε, measurable (F x))
+  (hF_meas : ∀ x ∈ ball x₀ ε, ae_measurable (F x) μ)
   (hF_int : integrable (F x₀) μ)
-  (hF'_meas : measurable F')
+  (hF'_meas : ae_measurable F' μ)
   (h_lipsch : ∀ᵐ a ∂μ, lipschitz_on_with (real.nnabs $ bound a) (λ x, F x a) (ball x₀ ε))
-  (bound_measurable : measurable (bound : α → ℝ))
   (bound_integrable : integrable (bound : α → ℝ) μ)
   (h_diff : ∀ᵐ a ∂μ, has_fderiv_at (λ x, F x a) (F' a) x₀) :
   integrable F' μ ∧ has_fderiv_at (λ x, ∫ a, F x a ∂μ) (∫ a, F' a ∂μ) x₀ :=
@@ -213,7 +232,7 @@ begin
   have x₀_in : x₀ ∈ ball x₀ ε := mem_ball_self ε_pos,
   have nneg : ∀ x, 0 ≤ ∥x - x₀∥⁻¹ := λ x, inv_nonneg.mpr (norm_nonneg _) ,
   set b : α → ℝ := λ a, abs (bound a),
-  have b_meas : measurable b :=  measurable_abs.comp bound_measurable,
+  --have b_meas : measurable b :=  measurable_abs.comp bound_measurable,
   have b_int : integrable b μ := bound_integrable.norm,
   have b_nonneg : ∀ a, 0 ≤ b a := λ a, abs_nonneg _,
   have hF_int' : ∀ x ∈ ball x₀ ε, integrable (F x) μ,
@@ -226,15 +245,14 @@ begin
       rw [mul_comm, nnreal.coe_nnabs, real.norm_eq_abs],
       rw [mem_ball, dist_eq_norm, norm_sub_rev] at x_in,
       exact mul_le_mul_of_nonneg_right (le_of_lt x_in) (abs_nonneg  _) },
-    apply integrable_of_norm_sub_le (hF_meas x₀ x₀_in) hF_int _ _ this,
-    exact measurable.const_mul (measurable_norm.comp bound_measurable) ε,
-    apply integrable.const_mul bound_integrable.norm },
+    exact integrable_of_norm_sub_le (hF_meas x x_in) hF_int
+      (integrable.const_mul bound_integrable.norm ε) this },
   have hF'_int : integrable F' μ,
   { have : ∀ᵐ a ∂μ, ∥F' a∥ ≤ b a,
     { apply (h_diff.and h_lipsch).mono,
       rintros a ⟨ha_diff, ha_lip⟩,
       exact ha_diff.le_of_lip (ball_mem_nhds _ ε_pos) ha_lip },
-    exact b_int.mono' this },
+    exact b_int.mono' hF'_meas this },
   refine ⟨hF'_int, _⟩,
   have h_ball: ball x₀ ε ∈ 𝓝 x₀ := ball_mem_nhds x₀ ε_pos,
   have : ∀ᶠ x in 𝓝 x₀,
@@ -243,22 +261,16 @@ begin
   { apply mem_sets_of_superset (ball_mem_nhds _ ε_pos),
     intros x x_in,
     rw [set.mem_set_of_eq, ← norm_smul_of_nonneg (nneg _), integral_smul,
-        integral_sub, integral_sub, continuous_linear_map.apply_integral hF'_meas hF'_int],
-    exacts [hF_meas _ x_in,
-            hF_int' x x_in,
-            hF_meas _ x₀_in,
-            hF_int,
-            (hF_meas _ x_in).sub (hF_meas _ x₀_in),
-            (hF_int' x x_in).sub (hF_meas _ x_in) (hF_meas _ x₀_in) hF_int,
-            hF'_meas.apply_continuous_linear_map _,
-            hF'_int.apply_continuous_linear_map hF'_meas _] },
+        integral_sub, integral_sub, continuous_linear_map.apply_integral hF'_int],
+    exacts [hF_int' x x_in, hF_int, (hF_int' x x_in).sub hF_int,
+            hF'_int.apply_continuous_linear_map _] },
   rw [has_fderiv_at_iff_tendsto, tendsto_congr' this, ← tendsto_zero_iff_norm_tendsto_zero,
       ← show ∫ (a : α), ∥x₀ - x₀∥⁻¹ • (F x₀ a - F x₀ a - (F' a) (x₀ - x₀)) ∂μ = 0, by simp],
   apply tendsto_integral_filter_of_dominated_convergence,
   { apply is_countably_generated_nhds },
   { filter_upwards [h_ball],
     intros x x_in,
-    apply measurable.const_smul,
+    apply ae_measurable.const_smul,
     exact ((hF_meas _ x_in).sub (hF_meas _ x₀_in)).sub (hF'_meas.apply_continuous_linear_map _) },
   { simp [measurable_const] },
   { apply mem_sets_of_superset h_ball,
@@ -280,7 +292,7 @@ begin
     by_cases h : ∥x - x₀∥ = 0,
     { simpa [h] using add_nonneg (b_nonneg a) (norm_nonneg (F' a)) },
     { field_simp [h] } },
-  { exact integrable.add b_meas b_int hF'_meas.norm hF'_int.norm },
+  { exact b_int.add hF'_int.norm },
   { apply h_diff.mono,
     intros a ha,
     suffices : tendsto (λ x, ∥x - x₀∥⁻¹ • (F x a - F x₀ a - F' a (x - x₀))) (𝓝 x₀) (𝓝 0),
@@ -296,27 +308,26 @@ lemma has_fderiv_at_of_dominated_loc_of_lip {F : H → α → E} {F' : α → (H
   {bound : α → ℝ}
   {ε : ℝ}
   (ε_pos : 0 < ε)
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) μ)
   (hF_int : integrable (F x₀) μ)
-  (hF'_meas : measurable F')
+  (hF'_meas : ae_measurable F' μ)
   (h_lip : ∀ᵐ a ∂μ, lipschitz_on_with (real.nnabs $ bound a) (λ x, F x a) (ball x₀ ε))
-  (bound_measurable : measurable (bound : α → ℝ))
   (bound_integrable : integrable (bound : α → ℝ) μ)
   (h_diff : ∀ᵐ a ∂μ, has_fderiv_at (λ x, F x a) (F' a) x₀) :
   integrable F' μ ∧ has_fderiv_at (λ x, ∫ a, F x a ∂μ) (∫ a, F' a ∂μ) x₀ :=
 begin
-  obtain ⟨ε', ε'_pos, h'⟩ : ∃ ε' > 0, ∀ x ∈ ball x₀ ε', measurable (F x),
+  obtain ⟨ε', ε'_pos, h'⟩ : ∃ ε' > 0, ∀ x ∈ ball x₀ ε', ae_measurable (F x) μ,
   by simpa using nhds_basis_ball.eventually_iff.mp hF_meas,
   set δ := min ε ε',
   have δ_pos : 0 < δ := lt_min ε_pos ε'_pos,
-  replace h' : ∀ x, x ∈ ball x₀ δ → measurable (F x),
+  replace h' : ∀ x, x ∈ ball x₀ δ → ae_measurable (F x) μ,
   { intros x x_in,
     exact h' _ (ball_subset_ball (min_le_right ε ε') x_in) },
   replace h_lip : ∀ᵐ (a : α) ∂μ, lipschitz_on_with (real.nnabs $ bound a) (λ x, F x a) (ball x₀ δ),
   { apply h_lip.mono,
     intros a lip,
     exact lip.mono (ball_subset_ball $ min_le_left ε ε') },
-  apply has_fderiv_at_of_dominated_loc_of_lip' δ_pos  ; assumption
+  apply has_fderiv_at_of_dominated_loc_of_lip' δ_pos ; assumption
 end
 
 
@@ -324,11 +335,10 @@ lemma has_fderiv_at_of_dominated_of_fderiv_le {F : H → α → E} {F' : H → �
   {bound : α → ℝ}
   {ε : ℝ}
   (ε_pos : 0 < ε)
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) μ)
   (hF_int : integrable (F x₀) μ)
-  (hF'_meas : measurable (F' x₀))
+  (hF'_meas : ae_measurable (F' x₀) μ)
   (h_bound : ∀ᵐ a ∂μ, ∀ x ∈ ball x₀ ε, ∥F' x a∥ ≤ bound a)
-  (bound_measurable : measurable (bound : α → ℝ))
   (bound_integrable : integrable (bound : α → ℝ) μ)
   (h_diff : ∀ᵐ a ∂μ, ∀ x ∈ ball x₀ ε, has_fderiv_at (λ x, F x a) (F' x a) x) :
   has_fderiv_at (λ x, ∫ a, F x a ∂μ) (∫ a, F' x₀ a ∂μ) x₀ :=
@@ -344,8 +354,8 @@ begin
     apply convex.lipschitz_on_with_of_norm_has_fderiv_within_le _ ha_bound (convex_ball _ _),
     intros x x_in,
     exact (ha_deriv x x_in).has_fderiv_within_at, },
-  exact (has_fderiv_at_of_dominated_loc_of_lip ε_pos hF_meas hF_int hF'_meas this
-        bound_measurable bound_integrable diff_x₀).2
+  exact (has_fderiv_at_of_dominated_loc_of_lip ε_pos hF_meas hF_int
+                                               hF'_meas this bound_integrable diff_x₀).2
 end
 
 open set
@@ -359,56 +369,55 @@ lemma has_fderiv_at_of_dominated_of_fderiv_le'' {F : H → ℝ → E} {F' : H �
   {bound : ℝ → ℝ}
   {ε : ℝ}
   (ε_pos : 0 < ε)
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) $ ν.restrict (Ι a b))
   (hF_int : interval_integrable (F x₀) ν a b)
-  (hF'_meas : measurable (F' x₀))
+  (hF'_meas : ae_measurable (F' x₀) $ ν.restrict (Ι a b))
   (h_bound : ∀ᵐ t ∂ν, t ∈ I a b → ∀ x ∈ ball x₀ ε, ∥F' x t∥ ≤ bound t)
-  (bound_measurable : measurable bound)
   (bound_integrable : interval_integrable bound ν a b)
   (h_diff : ∀ᵐ t ∂ν, t ∈ I a b → ∀ x ∈ ball x₀ ε, has_fderiv_at (λ x, F x t) (F' x t) x) :
   has_fderiv_at (λ x, ∫ t in a..b, F x t ∂ν) (∫ t in a..b, F' x₀ t ∂ν) x₀ :=
 begin
   erw ae_interval_oc_iff' at h_diff h_bound,
-  exact has_fderiv_at.sub
-    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.1 hF'_meas
-      h_bound.1 bound_measurable bound_integrable.1 h_diff.1)
-    (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas hF_int.2 hF'_meas
-      h_bound.2 bound_measurable bound_integrable.2 h_diff.2)
+  simp_rw [ae_measurable_interval_oc_iff, eventually_and] at hF_meas hF'_meas,
+  exact (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas.1 hF_int.1 hF'_meas.1 h_bound.1
+         bound_integrable.1 h_diff.1).sub
+        (has_fderiv_at_of_dominated_of_fderiv_le ε_pos hF_meas.2 hF_int.2 hF'_meas.2 h_bound.2
+         bound_integrable.2 h_diff.2)
 end
 
-
-instance : measurable_space (ℝ →L[ℝ] E) := borel _
-
-instance : borel_space (ℝ →L[ℝ] E) := ⟨rfl⟩
+lemma continuous.ae_measurable {α γ : Type*} [topological_space α] [measurable_space α]
+  [opens_measurable_space α] [topological_space γ] [measurable_space γ]
+  [borel_space γ] {f : α → γ} (h : continuous f) (μ : measure α): ae_measurable f μ :=
+⟨f, h.measurable, eventually_eq.refl _ _⟩
 
 lemma has_deriv_at_of_dominated_loc_of_lip {F : ℝ → α → E} {F' : α → E} {x₀ : ℝ} {bound : α → ℝ}
   {ε : ℝ} (ε_pos : 0 < ε)
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) μ)
   (hF_int : integrable (F x₀) μ)
-  (hF'_meas : measurable F')
+  (hF'_meas : ae_measurable F' μ)
   (h_lipsch : ∀ᵐ a ∂μ, lipschitz_on_with (real.nnabs $ bound a) (λ x, F x a) (ball x₀ ε))
-  (bound_measurable : measurable (bound : α → ℝ))
   (bound_integrable : integrable (bound : α → ℝ) μ)
   (h_diff : ∀ᵐ a ∂μ, has_deriv_at (λ x, F x a) (F' a) x₀) :
   (integrable F' μ) ∧ has_deriv_at (λ x, ∫ a, F x a ∂μ) (∫ a, F' a ∂μ) x₀ :=
 begin
-  cases has_fderiv_at_of_dominated_loc_of_lip ε_pos hF_meas hF_int
-    ((1 : ℝ →L[ℝ] ℝ).smul_rightL.continuous.measurable.comp hF'_meas) h_lipsch
-    bound_measurable bound_integrable h_diff with hF'_int key,
-  replace hF'_int : integrable F' μ, by  simpa [← integrable_norm_iff] using hF'_int,
+  have hm := (continuous_linear_map.smul_rightL ℝ ℝ E 1).continuous.measurable.comp_ae_measurable
+             hF'_meas,
+  cases has_fderiv_at_of_dominated_loc_of_lip ε_pos hF_meas hF_int hm h_lipsch bound_integrable
+    h_diff with hF'_int key,
+  replace hF'_int : integrable F' μ,
+  { rw [← integrable_norm_iff hm] at hF'_int,
+    simpa [integrable_norm_iff, hF'_meas] using hF'_int },
   refine ⟨hF'_int, _⟩,
   simp_rw has_deriv_at_iff_has_fderiv_at at h_diff ⊢,
-  change has_fderiv_at (λ (x : ℝ), integral μ (F x)) ((1 : ℝ →L[ℝ] ℝ).smul_rightL (∫ a, F' a ∂μ)) x₀,
-  rwa ←  ((1 : ℝ →L[ℝ] ℝ).smul_rightL : E →L[ℝ] _).integral_comp_comm hF'_meas hF'_int
+  rwa continuous_linear_map.integral_comp_comm _ hF'_int at key,
 end
 
 lemma has_deriv_at_of_dominated_loc_of_deriv_le {F : ℝ → α → E} {F' : ℝ → α → E} {x₀ : ℝ} {bound : α → ℝ} {ε : ℝ}
   (ε_pos : 0 < ε)
-  (hF_meas : ∀ᶠ x in 𝓝 x₀, measurable (F x))
+  (hF_meas : ∀ᶠ x in 𝓝 x₀, ae_measurable (F x) μ)
   (hF_int : integrable (F x₀) μ)
-  (hF'_meas : measurable (F' x₀))
+  (hF'_meas : ae_measurable (F' x₀) μ)
   (h_bound : ∀ᵐ a ∂μ, ∀ x ∈ ball x₀ ε, ∥F' x a∥ ≤ bound a)
-  (bound_measurable : measurable (bound : α → ℝ))
   (bound_integrable : integrable bound μ)
   (h_diff : ∀ᵐ a ∂μ, ∀ x ∈ ball x₀ ε, has_deriv_at (λ x, F x a) (F' x a) x) :
   (integrable (F' x₀) μ) ∧ has_deriv_at (λn, ∫ a, F n a ∂μ) (∫ a, F' x₀ a ∂μ) x₀ :=
@@ -424,7 +433,8 @@ begin
     apply convex.lipschitz_on_with_of_norm_has_deriv_within_le (convex_ball _ _)
     (λ x x_in, (ha_deriv x x_in).has_deriv_within_at) ha_bound },
   exact has_deriv_at_of_dominated_loc_of_lip ε_pos hF_meas hF_int hF'_meas this
-        bound_measurable bound_integrable diff_x₀
+        bound_integrable diff_x₀
 end
 
-end
+
+#lint
