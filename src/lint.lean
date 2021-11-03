b139c57df97f2b381a47c44ebf3dff9b8b994643
Floris van Doorn <fpvdoorn@gmail.com>
2021-11-03 17:40:59 +0100

    minor change

--- src/lint.lean
+++ src/lint.lean
@@ -1,9 +1,8 @@
 import tactic.basic
 
 open lean.parser tactic interactive
-/-- Linter for the sphere eversion project -/
+/-- Linter for the sphere eversion project. -/
 @[user_command] meta def lint_sphere_cmd (_ : parse $ tk "#lint_sphere") : lean.parser unit :=
-do str ← get_project_dir `lint_sphere_cmd 9, lint_cmd_aux (@lint_project str "my project")
+do str ← get_project_dir `lint_sphere_cmd 9,
+   lint_cmd_aux (@lint_project str "the sphere eversion project")
 reserve notation `#lint_sphere`
-
-
