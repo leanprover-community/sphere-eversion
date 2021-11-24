aa2d935e1f8d52e34f519a31b93842d77c4b3ba6
Patrick Massot <patrickmassot@free.fr>
2021-11-24 16:42:05 +0100

    Finish has_fderiv_at_parametric_primitive_of_times_cont_diff

--- src/parametric_interval_integral.lean
+++ src/parametric_interval_integral.lean
@@ -138,6 +138,27 @@ end
 
 end
 
+section lipschitz
+
+variables {α β γ : Type*} [pseudo_emetric_space α] [pseudo_emetric_space β] [pseudo_emetric_space γ]
+
+protected lemma lipschitz_on_with.comp {Kf Kg : ℝ≥0} {f : β → γ} {g : α → β} {s : set α} {t : set β}
+  (hf : lipschitz_on_with Kf f t) (hg : lipschitz_on_with Kg g s) (hst : g '' s ⊆ t) :
+  lipschitz_on_with (Kf * Kg) (f ∘ g) s :=
+assume x x_in y y_in,
+calc edist (f (g x)) (f (g y))
+    ≤ Kf * edist (g x) (g y) : hf (hst $ mem_image_of_mem g x_in) (hst $ mem_image_of_mem g y_in)
+... ≤ Kf * (Kg * edist x y) : ennreal.mul_left_mono (hg x_in y_in)
+... = (Kf * Kg : ℝ≥0) * edist x y : by rw [← mul_assoc, ennreal.coe_mul]
+
+lemma lipschitz_with_prod_mk_left (a : α) : lipschitz_with 1 (prod.mk a : β → α × β) :=
+λ x y, show max _ _ ≤ _, by simp
+
+lemma lipschitz_with_prod_mk_right (b : β) : lipschitz_with 1 (λ a : α, (a, b)) :=
+λ x y, show max _ _ ≤ _, by simp
+
+end lipschitz
+
 section
 
 variables {α E : Type*} [linear_order α] [measurable_space α]
@@ -948,8 +969,16 @@ lemma has_fderiv_at_parametric_primitive_of_times_cont_diff {F : H → ℝ → E
 begin
   set a₀ :=  min a t₀ - 1,
   set b₀ :=  max a t₀ + 1,
-  have ha : a ∈ Ioo a₀ b₀, sorry,
-  have ht₀ : t₀ ∈ Ioo a₀ b₀, sorry,
+  have ha : a ∈ Ioo a₀ b₀,
+  { dsimp [a₀, b₀],
+    split,
+    linarith [min_le_left a t₀],
+    linarith [le_max_left a t₀] },
+  have ht₀ : t₀ ∈ Ioo a₀ b₀,
+  { dsimp [a₀, b₀],
+    split,
+    linarith [min_le_right a t₀],
+    linarith [le_max_right a t₀] },
   have cpct : is_compact ((closed_ball x₀ 1).prod $ Icc a₀ b₀),
       from (proper_space.is_compact_closed_ball x₀ 1).prod is_compact_Icc,
   obtain ⟨M, M_nonneg, F_bound⟩ : ∃ M : ℝ, 0 ≤ M ∧ ∀ x ∈ ball x₀ 1, ∀ t ∈ Ioo a₀ b₀, ∥F x t∥ ≤ M,
@@ -958,13 +987,15 @@ begin
     exact λ x x_in t t_in, hM (x, t) ⟨ball_subset_closed_ball x_in, mem_Icc_of_Ioo t_in⟩ },
   obtain ⟨K, F_lip⟩ : ∃ K, ∀ t ∈ Ioo a₀ b₀, lipschitz_on_with K (λ x, F x t) (ball x₀ 1),
   { have conv : convex ℝ ((closed_ball x₀ 1).prod $ Icc  a₀ b₀),
-    {
-      sorry },
+      from (convex_closed_ball x₀ 1).prod (convex_Icc a₀ b₀),
     rcases hF.lipschitz_on_with conv cpct with ⟨K, hK⟩,
     use K,
     intros t t_in,
-    sorry },
-  /- have cont_x : ∀ x, continuous (F x),
+    rw [show (λ (x : H), F x t) = (uncurry F) ∘ (λ x : H, (x, t)), by { ext, simp }, ← mul_one K],
+    apply hK.comp ((lipschitz_with_prod_mk_right t).lipschitz_on_with $ ball x₀ 1),
+    rintros ⟨x, s⟩ ⟨x', hx, h⟩, cases h,
+    refine ⟨ball_subset_closed_ball hx, mem_Icc_of_Ioo t_in⟩ },
+  have cont_x : ∀ x, continuous (F x),
     from λ x, hF.continuous.comp (continuous.prod.mk x),
   have int_Icc : ∀ x, integrable_on (F x) (Icc a₀ b₀),
     from λ x, (cont_x x).integrable_on_compact is_compact_Icc,
@@ -993,6 +1024,6 @@ begin
     intros t t_in,
     rw nnabs_coe K,
     exact F_lip t t_in },
-  { exact integrable_on_const.mpr (or.inr measure_Ioo_lt_top) }   -/sorry
+  { exact integrable_on_const.mpr (or.inr measure_Ioo_lt_top) }
 end
 end
