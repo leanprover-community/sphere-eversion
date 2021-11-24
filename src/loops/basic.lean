8d54b0dc54672c50784f3fb4da4554b3df7cfd54
Floris van Doorn <fpvdoorn@gmail.com>
2021-11-24 14:58:11 +0100

    reorder mem_nhds_set

--- src/loops/basic.lean
+++ src/loops/basic.lean
@@ -34,24 +34,24 @@ def nhds_set (s : set X) : filter X :=
 Sup (nhds '' s)
 
 lemma mem_nhds_set {s t : set X} : s ∈ nhds_set t ↔
-  ∃ U ⊆ s, t ⊆ U ∧ is_open U :=
+  ∃ U, is_open U ∧ t ⊆ U ∧ U ⊆ s :=
 begin
   simp only [nhds_set, filter.mem_Sup, forall_apply_eq_imp_iff₂, mem_image, and_imp,
     exists_prop, forall_exists_index, mem_nhds_iff],
   split,
   { intros h, choose f h1f h2f h3f using h,
-    refine ⟨⋃ (x : α) (h : x ∈ t), f x h, _, _, _⟩,
-    { simpa only [Union_subset_iff] },
+    refine ⟨⋃ (x : X) (h : x ∈ t), f x h, _, _, _⟩,
+    { exact is_open_Union (λ x, is_open_Union (h2f x)) },
     { intros x hxt, simp only [mem_Union], exact ⟨x, hxt, h3f x hxt⟩ },
-    { exact is_open_Union (λ x, is_open_Union (h2f x)) } },
-  { rintro ⟨U, hUs, htU, hU⟩ x hxt, exact ⟨U, hUs, hU, htU hxt⟩ }
+    { simpa only [Union_subset_iff] } },
+  { rintro ⟨U, hU, htU, hUs⟩ x hxt, exact ⟨U, hUs, hU, htU hxt⟩ }
 end
 
 lemma is_open.mem_nhds_set {U s : set X} (hU : is_open U) : U ∈ nhds_set s ↔ s ⊆ U :=
 begin
   rw [mem_nhds_set], split,
-  { rintro ⟨V, hVU, htV, hV⟩, exact htV.trans hVU },
-  { intro hsU, exact ⟨U, subset.rfl, hsU, hU⟩ }
+  { rintro ⟨V, hV, htV, hVU⟩, exact htV.trans hVU },
+  { intro hsU, exact ⟨U, hU, hsU, subset.rfl⟩ }
 end
 
 end
