65fe97942d300c76e37e25e070f1ba67bcf45923
Floris van Doorn <fpvdoorn@gmail.com>
2022-01-07 17:17:10 +0100

    continue

--- src/to_mathlib/topology/nhds_set.lean
+++ src/to_mathlib/topology/nhds_set.lean
@@ -4,27 +4,24 @@ open set
 
 variables {X : Type*} [topological_space X]
 
+-- to set/basic
+lemma forall_mem_image {α β} {f : α → β} {s : set α} {p : β → Prop} :
+  (∀ y ∈ f '' s, p y) ↔ (∀ x ∈ s, p (f x)) :=
+by simp only [mem_image, and_imp, forall_exists_index, forall_apply_eq_imp_iff₂]
+
+lemma subset_interior_iff {s t : set X} : t ⊆ interior s ↔ ∃ U, is_open U ∧ t ⊆ U ∧ U ⊆ s :=
+⟨λ h, ⟨interior s, is_open_interior, h, interior_subset⟩,
+  λ ⟨U, hU, htU, hUs⟩, htU.trans (interior_maximal hUs hU)⟩
+
 /-- The filter of neighborhoods of a set in a topological space. -/
 def nhds_set (s : set X) : filter X :=
 Sup (nhds '' s)
 
-lemma mem_nhds_set {s t : set X} : s ∈ nhds_set t ↔
-  ∃ U, is_open U ∧ t ⊆ U ∧ U ⊆ s :=
-begin
-  simp only [nhds_set, filter.mem_Sup, forall_apply_eq_imp_iff₂, mem_image, and_imp,
-    exists_prop, forall_exists_index, mem_nhds_iff],
-  split,
-  { intros h, choose f h1f h2f h3f using h,
-    refine ⟨⋃ (x : X) (h : x ∈ t), f x h, _, _, _⟩,
-    { exact is_open_Union (λ x, is_open_Union (h2f x)) },
-    { intros x hxt, simp only [mem_Union], exact ⟨x, hxt, h3f x hxt⟩ },
-    { simpa only [Union_subset_iff] } },
-  { rintro ⟨U, hU, htU, hUs⟩ x hxt, exact ⟨U, hUs, hU, htU hxt⟩ }
-end
+lemma subset_interior_iff_mem_nhds_set {U s : set X} : s ⊆ interior U ↔ U ∈ nhds_set s :=
+by simp_rw [nhds_set, filter.mem_Sup, forall_mem_image, subset_interior_iff_nhds]
+
+lemma mem_nhds_set {s t : set X} : s ∈ nhds_set t ↔ ∃ U, is_open U ∧ t ⊆ U ∧ U ⊆ s :=
+by { rw [← subset_interior_iff_mem_nhds_set, subset_interior_iff] }
 
 lemma is_open.mem_nhds_set {U s : set X} (hU : is_open U) : U ∈ nhds_set s ↔ s ⊆ U :=
-begin
-  rw [mem_nhds_set], split,
-  { rintro ⟨V, hV, htV, hVU⟩, exact htV.trans hVU },
-  { intro hsU, exact ⟨U, hU, hsU, subset.rfl⟩ }
-end
+by rw [← subset_interior_iff_mem_nhds_set, interior_eq_iff_open.mpr hU]
