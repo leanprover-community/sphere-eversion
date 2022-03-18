b38e16d7b5ebb99f99b25e4381b3c2b45128e388
Floris van Doorn <fpvdoorn@gmail.com>
2022-03-18 16:23:01 +0100

    some cleanup in multilinear

--- src/to_mathlib/linear_algebra/multilinear.lean
+++ src/to_mathlib/linear_algebra/multilinear.lean
@@ -6,6 +6,21 @@ Multilinear map stuff that was meant as preliminaries for smooth functions gluin
 We no longer intend to use this file in the sphere eversion project.
 -/
 
+namespace function
+
+variables {ι : Sort*} [decidable_eq ι] {α β : ι → Type*}
+
+/-- Special case of `function.apply_update`. Useful for `rw`/`simp`. -/
+lemma update_fst (g : Π i, α i × β i) (i : ι) (v : α i × β i) (j : ι) :
+  (update g i v j).fst = update (λ k, (g k).fst) i v.fst j :=
+apply_update (λ _, prod.fst) g i v j
+
+/-- Special case of `function.apply_update`. Useful for `rw`/`simp`. -/
+lemma update_snd (g : Π i, α i × β i) (i : ι) (v : α i × β i) (j : ι) :
+  (update g i v j).snd = update (λ k, (g k).snd) i v.snd j :=
+apply_update (λ _, prod.snd) g i v j
+
+end function
 open function
 
 namespace multilinear_map
@@ -23,22 +38,9 @@ def coprod (L₁ : multilinear_map R M₁ M₃) (L₂ : multilinear_map R M₂ M
   multilinear_map R (λ i, M₁ i × M₂ i) M₃ :=
 { to_fun := λ v, L₁ (λ i, (v i).1) + L₂ (λ i, (v i).2),
   map_add' := λ v i p q, by {
-  have h1 := function.apply_update (λ _, prod.fst) v, dsimp at h1,
-  have h2 := function.apply_update (λ _, prod.snd) v, dsimp at h2,
-  simp_rw [h1, h2, add_add_add_comm, ← L₁.map_add, ← L₂.map_add, prod.add_def] },
+  simp_rw [update_fst, update_snd, add_add_add_comm, ← L₁.map_add, ← L₂.map_add, prod.add_def] },
   map_smul' := λ v i c p, by {
-  have h1 := function.apply_update (λ _, prod.fst) v, dsimp at h1,
-  have h2 := function.apply_update (λ _, prod.snd) v, dsimp at h2,
-  simp_rw [h1, h2, smul_add, ← L₁.map_smul, ← L₂.map_smul, prod.smul_def] } }
-
-/-- If `g` is a multilinear map and `f` is a collection of multilinear maps,
-then `g (f₁ m, ..., fₙ m)` is again a multilinear map, that we call
-`g.comp f`. -/
-def comp (g : multilinear_map R N M₃) (f : Π i, multilinear_map R M₁ (N i)) :
-  multilinear_map R M₁ M₃ :=
-{ to_fun := λ v, g (λ i, f i v),
-  map_add' := sorry,
-  map_smul' := sorry }
+  simp_rw [update_fst, update_snd, smul_add, ← L₁.map_smul, ← L₂.map_smul, prod.smul_def] } }
 
 end multilinear_map
 
@@ -57,20 +59,6 @@ def simps.apply (L₁ : continuous_multilinear_map R M₁ M₃) (v : Π i, M₁ 
 
 initialize_simps_projections continuous_multilinear_map
   (-to_multilinear_map, to_multilinear_map_to_fun → apply)
-
-@[simps]
-def comp (g : continuous_multilinear_map R N M₃) (f : Π i, continuous_multilinear_map R M₁ (N i)) :
-  continuous_multilinear_map R M₁ M₃ :=
-{ cont := sorry,
-  .. g.to_multilinear_map.comp $ λ i, (f i).to_multilinear_map }
-
-lemma comp_zero (g : continuous_multilinear_map R N M₃) :
-  g.comp (λ i, (0 : continuous_multilinear_map R M₁ (N i))) = 0 :=
-sorry
-
-lemma zero_comp (f : Π i, continuous_multilinear_map R M₁ (N i)) :
-  (0 : continuous_multilinear_map R N M₃).comp f = 0 :=
-sorry
 
 variables [has_continuous_add M₃]
 @[simps]
