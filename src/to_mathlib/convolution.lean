73b23fe96672acfe441ac6f1146c2b858e00f72e
Floris van Doorn <fpvdoorn@gmail.com>
2022-03-22 12:55:26 +0100

    more on bump functions

--- src/to_mathlib/convolution.lean
+++ src/to_mathlib/convolution.lean
@@ -959,26 +959,64 @@ end
 
 end normed_space
 
-
+namespace cont_diff_bump_of_inner
 section inner_product_space
 open finite_dimensional
 variables {f' f : G → E} {g' g : G → E'} {x' x : 𝕜} {n : with_top ℕ} [is_R_or_C 𝕜] [normed_group E] [normed_space 𝕜 E] [normed_group E'] [normed_space ℝ E'] [normed_space 𝕜 E'] [normed_group F] [normed_space ℝ F] [normed_space 𝕜 F] [smul_comm_class 𝕜 ℝ F] [inner_product_space ℝ G] [normed_space 𝕜 G] [smul_comm_class 𝕜 ℝ G] [complete_space E] [second_countable_topology E] [measurable_space E] [borel_space E] [complete_space E'] [second_countable_topology E'] [measurable_space E'] [borel_space E'] [complete_space F] [second_countable_topology F] [measurable_space F] [borel_space F] [measurable_space G] [borel_space G] [second_countable_topology G] [normed_group E''] [normed_space ℝ E''] [normed_space 𝕜 E''] [smul_comm_class 𝕜 ℝ E''] [complete_space E''] [second_countable_topology E''] [measurable_space E''] [borel_space E''] {μ : measure G} (L : E →L[𝕜] E' →L[𝕜] F)
-[is_add_left_invariant μ] [sigma_finite μ] [sigma_compact_space G] [proper_space G] [is_locally_finite_measure μ]
+[is_add_haar_measure μ] [sigma_compact_space G] [proper_space G]
 variables [finite_dimensional ℝ G]
 variables [second_countable_topology E'] [is_scalar_tower ℝ 𝕜 E']
-variables (φ : cont_diff_bump_of_inner (0 : G))
+variables {a : G} (φ : cont_diff_bump_of_inner (0 : G))
 
-def _root_.cont_diff_bump_of_inner.normed {a : G} (φ : cont_diff_bump_of_inner a) (μ : measure G)
-  (x : G) : ℝ :=
-φ x / ∫ x, φ x ∂μ
+lemma nonneg' (φ : cont_diff_bump_of_inner a) (x : G) : 0 ≤ φ x :=
+φ.nonneg
 
-lemma _root_.cont_diff_bump_of_inner.integral_normed {a : G} (φ : cont_diff_bump_of_inner a) :
+protected lemma continuous (φ : cont_diff_bump_of_inner a) : continuous φ :=
+cont_diff_zero.mp φ.cont_diff
+
+lemma tsupport_eq (φ : cont_diff_bump_of_inner a) : tsupport φ = closed_ball a φ.R :=
+by simp_rw [tsupport, φ.support_eq, closure_ball _ φ.R_pos]
+
+protected lemma has_compact_support (φ : cont_diff_bump_of_inner a) :
+  has_compact_support φ :=
+by simp_rw [has_compact_support, φ.tsupport_eq, is_compact_closed_ball]
+
+protected lemma integrable (φ : cont_diff_bump_of_inner a) : integrable φ μ :=
+φ.continuous.integrable_of_has_compact_support φ.has_compact_support
+
+lemma integral_pos (φ : cont_diff_bump_of_inner a) : 0 < ∫ x, φ x ∂μ :=
+begin
+  refine (integral_pos_iff_support_of_nonneg φ.nonneg' φ.integrable).mpr _,
+  rw [φ.support_eq],
+  refine is_open_ball.measure_pos _ (nonempty_ball.mpr φ.R_pos)
+end
+
+protected def normed (φ : cont_diff_bump_of_inner a) (μ : measure G) : G → ℝ :=
+λ x, φ x / ∫ x, φ x ∂μ
+
+lemma nonneg_normed (φ : cont_diff_bump_of_inner a) (x : G) : 0 ≤ φ.normed μ x :=
+div_nonneg φ.nonneg $ integral_nonneg φ.nonneg'
+
+lemma integral_normed (φ : cont_diff_bump_of_inner a) :
   ∫ x, φ.normed μ x ∂μ = 1 :=
-sorry
+begin
+  simp_rw [cont_diff_bump_of_inner.normed, div_eq_mul_inv, mul_comm (φ _), ← smul_eq_mul,
+    integral_smul],
+  exact inv_mul_cancel (φ.integral_pos.ne')
+end
 
-lemma _root_.cont_diff_bump_of_inner.normed.has_compact_support {a : G}
-  (φ : cont_diff_bump_of_inner a) : has_compact_support (φ.normed μ) :=
-sorry
+lemma support_normed_eq (φ : cont_diff_bump_of_inner a) :
+  support (φ.normed μ) = metric.ball a φ.R :=
+by simp_rw [cont_diff_bump_of_inner.normed, support_div, φ.support_eq,
+  support_const φ.integral_pos.ne', inter_univ]
+
+lemma tsupport_normed_eq (φ : cont_diff_bump_of_inner a) :
+  tsupport (φ.normed μ) = metric.closed_ball a φ.R :=
+by simp_rw [tsupport, φ.support_normed_eq, closure_ball _ φ.R_pos]
+
+lemma has_compact_support_normed (φ : cont_diff_bump_of_inner a) :
+  has_compact_support (φ.normed μ) :=
+by simp_rw [has_compact_support, φ.tsupport_normed_eq, is_compact_closed_ball]
 
 open continuous_linear_map
 lemma cont_diff_bump_of_inner.convolution_eq_right {x₀ : G}
@@ -993,6 +1031,7 @@ begin
 end
 
 end inner_product_space
+end cont_diff_bump_of_inner
 
 section normed_space
 
