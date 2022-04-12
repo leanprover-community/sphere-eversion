726a1327706d9d02b60f318bfe9354f91912b0fa
Patrick Massot <patrickmassot@free.fr>
2022-04-12 22:11:59 +0200

    Move away unused stuff and delete some dead code

--- src/to_mathlib/partition.lean
+++ src/to_mathlib/partition.lean
@@ -1,6 +1,6 @@
 import geometry.manifold.partition_of_unity
 
-import to_mathlib.geometry.manifold.cont_mdiff
+--import to_mathlib.geometry.manifold.cont_mdiff
 
 noncomputable theory
 
@@ -347,38 +347,6 @@ begin
 end
 
 open topological_space
-
-lemma exists_cont_diff_of_convex_of_is_open {s : set E} (hs : is_open s)
-  {P : E → F → Prop} (hP : ∀ x ∈ s, convex ℝ {y | P x y})
-  {n : with_top ℕ}
-  (hP' : ∀ x ∈ s, ∃ U ∈ 𝓝 x, ∃ f : E → F, cont_diff_on ℝ n f U ∧ ∀ x ∈ U, P x (f x)) :
-  ∃ f : E → F, cont_diff_on ℝ n f s ∧ ∀ x ∈ s, P x (f x) :=
-begin
-  classical,
-  let S : opens E := ⟨s, hs⟩,
-  suffices : ∃ f : S → F, cont_mdiff 𝓘(ℝ, E) 𝓘(ℝ, F) n f ∧ ∀ (x : ↥S), P x (f x),
-  { rcases this with ⟨f, hf, hf'⟩,
-    refine ⟨λ x, if hx : x ∈ s then f ⟨x, hx⟩ else 0, cont_mdiff_iff_cont_diff_on'.mp hf, _⟩,
-    intros x hx,
-    rw dif_pos hx,
-    exact hf' ⟨x, hx⟩ },
-  let PS : S → F → Prop := λ s y, P s y,
-  change ∃ f : S → F, cont_mdiff 𝓘(ℝ, E) 𝓘(ℝ, F) n f ∧ ∀ (x : ↥S), PS x (f x),
-  haveI : locally_compact_space S := hs.locally_compact_space,
-  haveI : t2_space S := subtype.t2_space,
-  apply exists_cont_mdiff_of_convex,
-  { rintros ⟨x, hx⟩,
-    exact hP x hx },
-  { rintros ⟨x, hx : x ∈ S⟩,
-    rcases hP' x hx with ⟨U, U_in, f, hf, hf'⟩,
-    refine ⟨coe ⁻¹' U, _, f ∘ coe, _, _⟩,
-    { rw nhds_subtype_eq_comap,
-      exact preimage_mem_comap U_in },
-    { rw cont_mdiff_on_iff_cont_diff_on',
-      exact hf.mono (s.inter_subset_right U) },
-    { rintros ⟨x, hx : x ∈ s⟩ (hx' : x ∈ U),
-      exact hf' x hx' } }
-end
 
 example {f : E → ℝ} (h : ∀ x : E, ∃ U ∈ 𝓝 x, ∃ ε : ℝ, ∀ x' ∈ U, 0 < ε ∧ ε ≤ f x') :
   ∃ f' : E → ℝ, cont_diff ℝ ⊤ f' ∧ ∀ x, (0 < f' x ∧ f' x ≤ f x) :=
