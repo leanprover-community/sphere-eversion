beda7434b2ea1174b473af11ea65a0b417020611
Yury G. Kudryashov <urkud@urkud.name>
2022-07-21 15:30:18 -0400

    Add a comment

--- src/to_mathlib/data/set/basic.lean
+++ src/to_mathlib/data/set/basic.lean
@@ -3,6 +3,7 @@ import data.set.basic
 namespace set
 
 -- Should this be some sort of `set.comap` result?
+-- This is just `equiv.set.sep.symm`
 def subsubset_equiv_inter {α : Type*} (s : set α) (P : α → Prop) :
   {x : s | P ↑x} ≃ ↥(s ∩ {x : α | P x}) :=
 { to_fun := λ x, ⟨x, mem_inter x.1.2 x.2⟩,
