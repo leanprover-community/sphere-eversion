3b940fd180acf6b03ba128c169bad2342b905bd4
Oliver Nash <github@olivernash.org>
2022-09-01 10:01:27 +0100

    De-sorry `nhds_has_basis_balls_of_open_cov`

--- src/to_mathlib/geometry/manifold/charted_space.lean
+++ src/to_mathlib/geometry/manifold/charted_space.lean
@@ -35,7 +35,18 @@ open metric (hiding mem_nhds_iff ball) set
 
 variables {E M H : Type*} [topological_space H] [topological_space M] [charted_space H M]
 variables [normed_add_comm_group E] [normed_space ℝ E]
-variables (I : model_with_corners ℝ E H)
+variables (I : model_with_corners ℝ E H) [model_with_corners.boundaryless I]
+
+lemma ext_chart_at_map_nhds_of_boundaryless {x : M} :
+  filter.map (ext_chart_at I x) (𝓝 x) = 𝓝 (ext_chart_at I x x) :=
+by rw [ext_chart_at_map_nhds I x, model_with_corners.boundaryless.range_eq_univ, nhds_within_univ]
+
+lemma ext_chart_at_image_nhd_mem_nhds_of_boundaryless {x : M} {s : set M} (h : s ∈ 𝓝 x) :
+  (ext_chart_at I x) '' s ∈ 𝓝 (ext_chart_at I x x) :=
+begin
+  rw [← ext_chart_at_map_nhds_of_boundaryless, filter.mem_map],
+  filter_upwards [h] using subset_preimage_image (ext_chart_at I x) s,
+end
 
 def ball (x : M) (r : ℝ) := (ext_chart_at I x).symm '' metric.ball (ext_chart_at I x x) r
 
@@ -46,24 +57,19 @@ lemma nhds_has_basis_balls_of_open_cov (x : M)
                          ∃ j, charted_space.ball I x r ⊆ s j)
                    (charted_space.ball I x) :=
 begin
-  sorry,
-/- Old proof: could assume `M` was charted over `E` and so use `chart_at` instead of `ext_chart_at`
-
   -- MASSIVE golfing opportunity!
   obtain ⟨j, hj⟩ : ∃ j, x ∈ s j, by { simpa only [mem_Union, ← cov] using mem_univ x, },
   replace hj : s j ∈ 𝓝 x := mem_nhds_iff.mpr ⟨s j, subset.rfl, s_op j, hj⟩,
-  have hx : (chart_at E x).source ∈ 𝓝 x := -- cf `ext_chart_at_source_mem_nhds`
-    is_open.mem_nhds (chart_at E x).open_source (mem_chart_source E x),
+  have hx : (ext_chart_at I x).source ∈ 𝓝 x := ext_chart_at_source_mem_nhds I x,
   refine filter.has_basis_iff.mpr (λ n, ⟨λ hn, _, _⟩),
-  { let m := s j ∩ n ∩ (chart_at E x).to_local_equiv.source,
+  { let m := s j ∩ n ∩ (ext_chart_at I x).source,
     have hm : m ∈ 𝓝 x := filter.inter_mem (filter.inter_mem hj hn) hx,
-    replace hm : (chart_at E x) '' m ∈ 𝓝 (chart_at E x x),
-    { rw ← (chart_at E x).map_nhds_eq (mem_chart_source E x),
-      exact filter.image_mem_map hm, },
+    replace hm : (ext_chart_at I x) '' m ∈ 𝓝 (ext_chart_at I x x) :=
+      ext_chart_at_image_nhd_mem_nhds_of_boundaryless I hm,
     obtain ⟨r, hr₀, hr₁⟩ :=
-      (filter.has_basis_iff.mp (@nhds_basis_ball E _ (chart_at E x x)) _).mp hm,
+      (filter.has_basis_iff.mp (@nhds_basis_ball E _ (ext_chart_at I x x)) _).mp hm,
     refine ⟨r, ⟨hr₀, hr₁.trans _, ⟨j, _⟩⟩, _⟩,
-    { exact ((chart_at E x).maps_to.mono (inter_subset_right _ _) subset.rfl).image_subset },
+    { exact ((ext_chart_at I x).maps_to.mono (inter_subset_right _ _) subset.rfl).image_subset },
     { suffices : m ⊆ s j,
       { refine subset.trans _ this,
         convert monotone_image hr₁,
@@ -77,12 +83,11 @@ begin
           (set.inter_subset_right _ _)).symm, },
       exact (set.inter_subset_left _ _).trans (set.inter_subset_right _ _), }, },
   { rintros ⟨r, ⟨hr₀, hr₁, -⟩, hr₂⟩,
-    replace hr₀ : metric.ball (chart_at E x x) r ∈ 𝓝 (chart_at E x x) := ball_mem_nhds _ hr₀,
-    rw [← (chart_at E x).map_nhds_eq (mem_chart_source E x), filter.mem_map] at hr₀,
+    replace hr₀ : metric.ball (ext_chart_at I x x) r ∈ 𝓝 (ext_chart_at I x x) := ball_mem_nhds _ hr₀,
+    rw [← ext_chart_at_map_nhds_of_boundaryless, filter.mem_map] at hr₀,
     replace hr₀ := filter.inter_mem hx hr₀,
-    rw ← (chart_at E x).symm_image_eq_source_inter_preimage hr₁ at hr₀,
+    rw ← (ext_chart_at I x).symm_image_eq_source_inter_preimage hr₁ at hr₀,
     filter_upwards [hr₀] using hr₂, },
--/
 end
 
 end normed_add_comm_group
