88a02d3745eae07877e705766aa3bd7defa239d4
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-09-07 18:50:49 -0400

    "volume_form" independent of choice of basis

--- src/to_mathlib/analysis/inner_product_space.lean
+++ src/to_mathlib/analysis/inner_product_space.lean
@@ -1,4 +1,5 @@
 import analysis.inner_product_space.orientation
+import to_mathlib.analysis.inner_product_space.unitary_group
 
 noncomputable theory
 
@@ -6,12 +7,52 @@ open_locale real_inner_product_space big_operators
 open finite_dimensional
 
 variables {E : Type*} [inner_product_space ℝ E] [finite_dimensional ℝ E]
-  {n : ℕ} [fact (finrank ℝ E = n + 1)] (ω : orientation ℝ E (fin n.succ))
+
+section
+variables {ι : Type*} [fintype ι] [decidable_eq ι] (a b : orthonormal_basis ι ℝ E)
+
+lemma orthonormal_basis.det_to_matrix_orthonormal_basis_of_same_orientation
+  (h : a.to_basis.orientation = b.to_basis.orientation) :
+  a.to_basis.det b = 1 :=
+begin
+  have : a.to_basis.det b = -1 ∨ a.to_basis.det b = 1,
+  { simpa using a.det_to_matrix_orthonormal_basis_real b },
+  apply this.resolve_left,
+  have : 0 < a.to_basis.det b,
+  { rw a.to_basis.orientation_eq_iff_det_pos at h,
+    simpa using h },
+  linarith,
+end
+
+lemma orthonormal_basis.det_eq_det_of_same_orientation
+  (h : a.to_basis.orientation = b.to_basis.orientation) :
+  a.to_basis.det = b.to_basis.det :=
+begin
+  rw a.to_basis.det.eq_smul_basis_det b.to_basis,
+  simp [a.det_to_matrix_orthonormal_basis_of_same_orientation b h],
+end
+
+end
 
 namespace orientation
 
+variables {n : ℕ} [fact (finrank ℝ E = n + 1)] (ω : orientation ℝ E (fin n.succ))
+
 def volume_form : alternating_map ℝ E ℝ (fin n.succ) :=
-(ω.fin_orthonormal_basis n.succ_pos (fact.out _)).det
+(ω.fin_orthonormal_basis n.succ_pos (fact.out (finrank ℝ E = n + 1))).det
+
+lemma volume_form_robust (b : orthonormal_basis (fin n.succ) ℝ E) (hb : b.to_basis.orientation = ω) :
+  b.to_basis.det = ω.volume_form :=
+begin
+  let e : orthonormal_basis (fin n.succ) ℝ E :=
+    (ω.fin_orthonormal_basis n.succ_pos (fact.out _)).to_orthonormal_basis
+    (ω.fin_orthonormal_basis_orthonormal _ _),
+  apply b.det_eq_det_of_same_orientation e,
+  rw [hb, ← ω.fin_orthonormal_basis_orientation n.succ_pos (fact.out _)],
+  refl,
+end
+
+attribute [irreducible] orientation.volume_form
 
 lemma abs_volume_form_apply_le (v : fin n.succ → E) :
   |ω.volume_form v| ≤ ∏ i : fin n.succ, ∥v i∥ :=
@@ -31,5 +72,3 @@ lemma abs_volume_form_apply_of_orthonormal {v : fin n.succ → E} (hv : orthonor
 by simp [ω.abs_volume_form_apply_of_pairwise_orthogonal (λ i j, hv.2), hv.1]
 
 end orientation
-
-attribute [irreducible] orientation.volume_form
