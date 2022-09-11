c4e6679864e6b0009fe60519be42ecfa789011d6
Patrick Massot <patrickmassot@free.fr>
2022-09-11 18:38:39 +0200

    More slice lemmas

--- src/local/relation.lean
+++ src/local/relation.lean
@@ -35,8 +35,9 @@ noncomputable theory
 open set function module (dual) real filter
 open_locale unit_interval topological_space
 
-variables (E : Type*) [normed_add_comm_group E] [normed_space ℝ E] (F : Type*)
-                        [normed_add_comm_group F] [normed_space ℝ F]
+variables (E : Type*) [normed_add_comm_group E] [normed_space ℝ E]
+variables (F : Type*) [normed_add_comm_group F] [normed_space ℝ F]
+variables (P : Type*) [normed_add_comm_group P] [normed_space ℝ P]
 
 
 @[derive metric_space]
@@ -57,6 +58,10 @@ the set of `w` in `F` such that updating `θ` using `p` and `w` leads to a jet i
 def slice (R : rel_loc E F) (p : dual_pair' E) (θ : E × F × (E →L[ℝ] F)) : set F :=
 {w | (θ.1, θ.2.1, p.update θ.2.2 w) ∈ R}
 
+lemma mem_slice (R : rel_loc E F) {p : dual_pair' E} {θ : E × F × (E →L[ℝ] F)} {w : F} :
+  w ∈ R.slice p θ ↔ (θ.1, θ.2.1, p.update θ.2.2 w) ∈ R :=
+iff.rfl
+
 /-- A relation is ample if all its slices are ample. -/
 def is_ample (R : rel_loc E F) : Prop := ∀ (p : dual_pair' E) (θ : E × F × (E →L[ℝ] F)),
 ample_set (R.slice p θ)
@@ -72,6 +77,55 @@ begin
   cases θ,
   cases θ_snd,
   exact hθ
+end
+
+lemma slice_update {R : rel_loc E F} {θ : E × F × (E →L[ℝ] F)}
+  {p : dual_pair' E} (x : F) :
+  R.slice p (θ.1, θ.2.1, (p.update θ.2.2 x)) = R.slice p θ :=
+begin
+  ext1 w,
+  dsimp [slice],
+  rw [p.update_update]
+end
+
+/-- In order to check ampleness, it suffices to consider slices through elements of the relation. -/
+lemma is_ample_iff {R : rel_loc E F} : R.is_ample ↔
+  ∀ ⦃θ : one_jet E F⦄ (p : dual_pair' E), θ ∈ R → ample_set (R.slice p θ) :=
+begin
+  simp_rw [is_ample],
+  refine ⟨λ h θ p hθ, h p θ, λ h p θ w hw, _⟩,
+  dsimp [slice] at hw,
+  have := h p hw,
+  rw [slice_update] at this,
+  exact this w hw
+end
+
+
+open_locale pointwise
+
+lemma slice_of_ker_eq_ker {R : rel_loc E F} {θ : one_jet E F}
+  {p p' : dual_pair' E} (hpp' : p.π = p'.π) :
+  R.slice p θ = θ.2.2 (p.v - p'.v) +ᵥ R.slice p' θ :=
+begin
+  rcases θ with ⟨x, y, φ⟩,
+  have key : ∀ w, p'.update φ w = p.update φ (w + φ (p.v - p'.v)),
+  { intros w,
+    simp only [dual_pair'.update, hpp', map_sub, add_right_inj],
+    congr' 2,
+    abel },
+  ext w,
+  simp only [slice, mem_set_of_eq, map_sub, vadd_eq_add, mem_vadd_set_iff_neg_vadd_mem, key],
+  have : -(φ p.v - φ p'.v) + w + (φ p.v - φ p'.v) = w,
+  abel,
+  rw this,
+end
+
+lemma ample_slice_of_ample_slice {R : rel_loc E F} {θ : one_jet E F}
+  {p p' : dual_pair' E} (hpp' : p.π = p'.π) (h : ample_set (R.slice p θ)) :
+  ample_set (R.slice p' θ) :=
+begin
+  rw slice_of_ker_eq_ker hpp'.symm,
+  exact ample_set.vadd h
 end
 
 /-- A solution to a local relation `R`. -/
