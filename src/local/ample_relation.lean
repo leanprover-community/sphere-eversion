5c0a4a8601089f034f5a1efdf68a64752264da84
Patrick Massot <patrickmassot@free.fr>
2022-09-12 17:17:26 +0200

    Cleanup local.ample_relation

--- src/local/ample_relation.lean
+++ src/local/ample_relation.lean
@@ -2,13 +2,32 @@ import local.ample_set
 import local.dual_pair
 import local.relation
 
+/-! # Slices of first order relations
+
+Recal that a first order partial differential relation for maps between real normed vector spaces
+`E` and `F` is a set `R` in `one_jet E F := E × F × (E →L[ℝ] F)`. In this file we study slices
+of such relations. The word slice is meant to convey the idea of intersecting with an affine
+subspace. Here we fix `(x, y, φ) : one_jet E F` and some hyperplane `H` in `E`. The points
+`x` and `y` are fixed and we will take a slice in `E →L[ℝ] F` by intersecting `R` with the affine
+subspace of linear maps that coincide with `φ` on `H`.
+
+It will be convenient for convex integration purposes to identify this slice with `F`. There is
+no natural identification but we can build one by fixing more data that a hyperplane in `E`.
+Namely we fix `p : dual_pair E` (where `ker p.π` is the relevant hyperplane) and reformulate
+"linear map that coincides with `φ` on `H`" as `p.update φ w` for some `w : F`.
+
+This `slice` definition allows to define `rel_loc.is_ample`, the ampleness condition for first
+order relations: a relation is ample if all its slices are ample sets.
+
+At the end of the file we consider 1-jet sections and slices corresponding to points in their image.
+-/
+
 variables {E : Type*} [normed_add_comm_group E] [normed_space ℝ E]
 variables {F : Type*} [normed_add_comm_group F] [normed_space ℝ F]
-variables (P : Type*) [normed_add_comm_group P] [normed_space ℝ P]
-
 
 open set
 
+/-! ## Slices and ampleness -/
 namespace rel_loc
 
 /-- The slice of a local relation `R : rel_loc E F` for a dual pair `p` at a jet `θ` is
@@ -24,16 +43,12 @@ iff.rfl
 def is_ample (R : rel_loc E F) : Prop := ∀ (p : dual_pair' E) (θ : E × F × (E →L[ℝ] F)),
 ample_set (R.slice p θ)
 
-/- FIXME: the proof below is awful. -/
 lemma is_ample.mem_hull {R : rel_loc E F} (h : is_ample R) {θ : E × F × (E →L[ℝ] F)}
   (hθ : θ ∈ R) (v : F) (p) : v ∈ hull (connected_component_in (R.slice p θ) (θ.2.2 p.v)) :=
 begin
-  rw h p θ (θ.2.2 p.v) _,
+  rw h p θ (θ.2.2 p.v),
   exact mem_univ _,
-  dsimp [rel_loc.slice],
-  rw p.update_self,
-  cases θ,
-  cases θ_snd,
+  rw [mem_slice, p.update_self, prod.mk.eta, prod.mk.eta],
   exact hθ
 end
 
@@ -90,16 +105,28 @@ end rel_loc
 
 open rel_loc
 
-namespace jet_sec
-
 variables  {R : rel_loc E F}
+
+/-! ## Slices for 1-jet sections and formal solutions. -/
+
+namespace jet_sec
 
 /-- The slice associated to a jet section and a dual pair at some point. -/
 def slice_at (𝓕 : jet_sec E F) (R : rel_loc E F) (p : dual_pair' E) (x : E) : set F :=
 R.slice p (x, 𝓕.f x, 𝓕.φ x)
 
+/-- A 1-jet section `𝓕` is short for a dual pair `p` at a point `x` if the derivative of
+the function `𝓕.f` at `x` is in the convex hull of the relevant connected component of the
+corresponding slice. -/
+def is_short_at (𝓕 : jet_sec E F) (R : rel_loc E F) (p : dual_pair' E) (x : E) : Prop :=
+D 𝓕.f x p.v ∈ hull (connected_component_in (𝓕.slice_at R p x) $ 𝓕.φ x p.v)
+
+end jet_sec
+
+namespace rel_loc.formal_sol
+
 /-- The slice associated to a formal solution and a dual pair at some point. -/
-def _root_.rel_loc.formal_sol.slice_at (𝓕 : formal_sol R) (p : dual_pair' E) (x : E) : set F :=
+def slice_at (𝓕 : formal_sol R) (p : dual_pair' E) (x : E) : set F :=
 R.slice p (x, 𝓕.f x, 𝓕.φ x)
 
 lemma mem_slice (𝓕 : formal_sol R) (p : dual_pair' E) {x : E} :
@@ -109,15 +136,11 @@ by simpa [rel_loc.formal_sol.slice_at, rel_loc.slice] using  𝓕.is_sol x
 /-- A formal solution `𝓕` is short for a dual pair `p` at a point `x` if the derivative of
 the function `𝓕.f` at `x` is in the convex hull of the relevant connected component of the
 corresponding slice. -/
-def is_short_at (𝓕 : jet_sec E F) (R : rel_loc E F) (p : dual_pair' E) (x : E) : Prop :=
-D 𝓕.f x p.v ∈ hull (connected_component_in (𝓕.slice_at R p x) $ 𝓕.φ x p.v)
-
-def _root_.rel_loc.formal_sol.is_short_at (𝓕 : formal_sol R)(p : dual_pair' E) (x : E) : Prop :=
+def is_short_at (𝓕 : formal_sol R) (p : dual_pair' E) (x : E) : Prop :=
 D 𝓕.f x p.v ∈ hull (connected_component_in (𝓕.slice_at p x) $ 𝓕.φ x p.v)
 
-lemma _root_.rel_loc.is_ample.is_short_at {R : rel_loc E F} (hR : is_ample R) (𝓕 : formal_sol R) (p : dual_pair' E)
+end rel_loc.formal_sol
+
+lemma rel_loc.is_ample.is_short_at (hR : is_ample R) (𝓕 : formal_sol R) (p : dual_pair' E)
   (x : E) : 𝓕.is_short_at p x :=
 hR.mem_hull (𝓕.is_sol x) _ p
-
-
-end jet_sec
