6cc1e5790a35836f78a46c3e55a0ce500c73368c
Patrick Massot <patrickmassot@free.fr>
2023-03-30 17:12:01 +0200

    Finish inductive constructions.

--- src/to_mathlib/data/set/lattice.lean
+++ src/to_mathlib/data/set/lattice.lean
@@ -12,4 +12,19 @@ lemma const_prod_Union₂ {κ : ι → Sort*} {s : Π i, κ i → set α} {t : s
   t ×ˢ (⋃ i j, s i j) = ⋃ i j, t ×ˢ s i j :=
 by simp_rw [const_prod_Union]
 
+lemma bUnion_le {α ι : Type*} [partial_order ι] (s : ι → set α) (i : ι) :
+  (⋃ j ≤ i, s j) = (⋃ j < i, s j) ∪ s i :=
+begin
+  simp only [(λ j, le_iff_lt_or_eq : ∀ j, j ≤ i ↔ j < i ∨ j = i)],
+  erw [bUnion_union, bUnion_singleton],
+  refl
+end
+
+lemma bUnion_ge {α ι : Type*} [partial_order ι] (s : ι → set α) (i : ι) :
+  (⋃ j ≥ i, s j) = s i ∪ (⋃ j > i, s j) :=
+begin
+  erw [@bUnion_le α (order_dual ι) _ s i, union_comm],
+  refl
+end
+
 end set
