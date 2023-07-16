e3661983fc1186a0bb0983b6d204b012d8ca607f
Yury G. Kudryashov <urkud@urkud.name>
2023-07-16 16:53:09 -0500

    Golf

--- src/to_mathlib/order/filter/basic.lean
+++ src/to_mathlib/order/filter/basic.lean
@@ -2,9 +2,5 @@ import order.filter.basic
 
 lemma filter.eventually_eq.eventually_eq_ite {X Y : Type*} {l : filter X} {f g : X → Y}
   {P : X → Prop} [decidable_pred P] (h : f =ᶠ[l] g) :
-(λ x, if P x then f x else g x) =ᶠ[l] f :=
-begin
-  apply h.mono (λ x hx, _),
-  dsimp only,
-  split_ifs ; tauto
-end
+  (λ x, if P x then f x else g x) =ᶠ[l] f :=
+h.mono $ λ x hx, by simp [hx]
