911a46f321d645bed98153da184046967c27b240
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 15:43:21 -0500

    Fix a file

--- SphereEversion/ToMathlib/Logic/Equiv/LocalEquiv.lean
+++ SphereEversion/ToMathlib/Logic/Equiv/LocalEquiv.lean
@@ -3,6 +3,5 @@ import Mathlib.Logic.Equiv.LocalEquiv
 open Set
 
 theorem LocalEquiv.range_eq_target_of_source_eq_univ {α β : Type _} (e : LocalEquiv α β)
-    (h : e.source = univ) : range e = e.target := by rw [← image_univ, ← h];
-  exact e.image_source_eq_target
-
+    (h : e.source = univ) : range e = e.target := by
+  rw [← image_univ, ← h, e.image_source_eq_target]
