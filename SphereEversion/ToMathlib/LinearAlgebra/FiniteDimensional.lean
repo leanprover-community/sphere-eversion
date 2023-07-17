ae25545f22f057bf97cc08798ea0da49c19d6a9d
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 15:48:04 -0500

    Fix +1 file

--- SphereEversion/ToMathlib/LinearAlgebra/FiniteDimensional.lean
+++ SphereEversion/ToMathlib/LinearAlgebra/FiniteDimensional.lean
@@ -6,18 +6,16 @@ variable {𝕜 : Type _} [Field 𝕜] {E : Type _} [AddCommGroup E] [Module 𝕜
   [AddCommGroup E'] [Module 𝕜 E']
 
 theorem two_le_rank_of_rank_lt_rank [FiniteDimensional 𝕜 E] [FiniteDimensional 𝕜 E'] {π : E →ₗ[𝕜] 𝕜}
-    (hπ : π.ker ≠ ⊤) (h : finrank 𝕜 E < finrank 𝕜 E') (φ : E →ₗ[𝕜] E') :
-    2 ≤ Module.rank 𝕜 (E' ⧸ Submodule.map φ π.ker) :=
-  by
-  suffices 2 ≤ finrank 𝕜 (E' ⧸ π.ker.map φ)
-    by
-    rw [← finrank_eq_rank]
+    (hπ : LinearMap.ker π ≠ ⊤) (h : finrank 𝕜 E < finrank 𝕜 E') (φ : E →ₗ[𝕜] E') :
+    2 ≤ Module.rank 𝕜 (E' ⧸ Submodule.map φ (LinearMap.ker π)) := by
+  suffices : 2 ≤ finrank 𝕜 (E' ⧸ π.ker.map φ)
+  · rw [← finrank_eq_rank]
     exact_mod_cast this
   apply le_of_add_le_add_right
   rw [Submodule.finrank_quotient_add_finrank (π.ker.map φ)]
   have :=
     calc
-      finrank 𝕜 (π.ker.map φ) ≤ finrank 𝕜 π.ker := finrank_map_le φ π.ker
+      finrank 𝕜 (π.ker.map φ) ≤ finrank 𝕜 (LinearMap.ker π) := finrank_map_le φ (LinearMap.ker π)
       _ < finrank 𝕜 E := Submodule.finrank_lt (le_top.lt_of_ne hπ)
   linarith
 
