b8a3814b0a37802facebf3a8b39e7401249cbec7
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 15:36:59 -0500

    Fix more

--- SphereEversion/Local/AmpleSet.lean
+++ SphereEversion/Local/AmpleSet.lean
@@ -48,18 +48,16 @@ def AmpleSet (s : Set F) : Prop :=
   ∀ x ∈ s, convexHull ℝ (connectedComponentIn s x) = univ
 
 /-- images of ample sets under continuous linear equivalences are ample. -/
-theorem AmpleSet.image {s : Set E} (h : AmpleSet s) (L : E ≃L[ℝ] F) : AmpleSet (L '' s) :=
-  by
-  intro x hx
+theorem AmpleSet.image {s : Set E} (h : AmpleSet s) (L : E ≃L[ℝ] F) :
+    AmpleSet (L '' s) := fun x hx ↦ by
   rw [L.image_eq_preimage] at hx 
-  have : L '' connectedComponentIn s (L.symm x) = connectedComponentIn (L '' s) x :=
-    by
+  have : L '' connectedComponentIn s (L.symm x) = connectedComponentIn (L '' s) x := by
     conv_rhs => rw [← L.apply_symm_apply x]
-    exact L.to_homeomorph.image_connected_component_in hx
+    exact L.toHomeomorph.image_connectedComponentIn hx
   rw [← this]
-  refine' (L.to_linear_equiv.to_linear_map.convex_hull_image _).trans _
+  refine' (L.toLinearEquiv.toLinearMap.convexHull_image _).trans _
   rw [h (L.symm x) hx, image_univ]
-  exact L.to_linear_equiv.to_equiv.range_eq_univ
+  exact L.surjective.range_eq
 
 -- unused
 /-- preimages of ample sets under continuous linear equivalences are ample. -/
@@ -110,8 +108,7 @@ attribute [local instance 100] TopologicalAddGroup.pathConnectedSpace
 is path connected in `p` then the complement of `q` is path connected in `F`. -/
 theorem isPathConnected_compl_of_isPathConnected_compl_zero [TopologicalAddGroup F]
     [ContinuousSMul ℝ F] {p q : Submodule ℝ F} (hpq : IsCompl p q)
-    (hpc : IsPathConnected ({0}ᶜ : Set p)) : IsPathConnected (qᶜ : Set F) :=
-  by
+    (hpc : IsPathConnected ({0}ᶜ : Set p)) : IsPathConnected (qᶜ : Set F) := by
   rw [isPathConnected_iff] at hpc ⊢
   constructor
   · rcases hpc.1 with ⟨a, ha⟩
@@ -124,11 +121,8 @@ theorem isPathConnected_compl_of_isPathConnected_compl_zero [TopologicalAddGroup
     let γ₂ := PathConnectedSpace.somePath (π hpq.symm x) (π hpq.symm y)
     let γ₁' : Path (_ : F) _ := γ₁.map continuous_subtype_val
     let γ₂' : Path (_ : F) _ := γ₂.map continuous_subtype_val
-    refine'
-      ⟨(γ₁'.add γ₂').cast (Submodule.linear_proj_add_linearProjOfIsCompl_eq_self hpq x).symm
-          (Submodule.linear_proj_add_linearProjOfIsCompl_eq_self hpq y).symm,
-        _⟩
-    intro t
+    refine ⟨(γ₁'.add γ₂').cast (Submodule.linear_proj_add_linearProjOfIsCompl_eq_self hpq x).symm
+      (Submodule.linear_proj_add_linearProjOfIsCompl_eq_self hpq y).symm, fun t ↦ ?_⟩
     rw [Path.cast_coe, Path.add_apply]
     change (γ₁ t : F) + (γ₂ t : F) ∉ q
     rw [← Submodule.linearProjOfIsCompl_apply_eq_zero_iff hpq, LinearMap.map_add,
@@ -139,10 +133,9 @@ theorem isPathConnected_compl_of_isPathConnected_compl_zero [TopologicalAddGroup
 /-- For `x` and `y` in a real vector space, if `x ≠ 0` and `0` is in the segment from
 `x` to `y` then `y` is on the line spanned by `x`.  -/
 theorem mem_span_of_zero_mem_segment {F : Type _} [AddCommGroup F] [Module ℝ F] {x y : F}
-    (hx : x ≠ 0) (h : (0 : F) ∈ [x -[ℝ] y]) : y ∈ Submodule.span ℝ ({x} : Set F) :=
-  by
+    (hx : x ≠ 0) (h : (0 : F) ∈ [x -[ℝ] y]) : y ∈ Submodule.span ℝ ({x} : Set F) := by
   rw [segment_eq_image] at h 
-  rcases h with ⟨t, ht, htxy⟩
+  rcases h with ⟨t, -, htxy⟩
   rw [Submodule.mem_span_singleton]
   dsimp only at htxy 
   use (t - 1) / t
@@ -154,7 +147,7 @@ theorem mem_span_of_zero_mem_segment {F : Type _} [AddCommGroup F] [Module ℝ F
   rw [← smul_eq_zero_iff_eq' (neg_ne_zero.mpr <| inv_ne_zero this), smul_add, smul_smul, smul_smul,
     ← neg_one_mul, mul_assoc, mul_assoc, inv_mul_cancel this, mul_one, neg_one_smul,
     add_neg_eq_zero] at htxy 
-  convert htxy
+  convert htxy using 2
   ring
 
 variable [TopologicalAddGroup F] [ContinuousSMul ℝ F]
@@ -162,33 +155,27 @@ variable [TopologicalAddGroup F] [ContinuousSMul ℝ F]
 /-- For `x` and `y` in a real vector space, if `x ≠ 0` and `y` is not on the line
 spanned by `x` then `x` and `y` can be joined by a path in the complement of `{0}`.  -/
 theorem joinedIn_compl_zero_of_not_mem_span {x y : F} (hx : x ≠ 0)
-    (hy : y ∉ Submodule.span ℝ ({x} : Set F)) : JoinedIn ({0}ᶜ : Set F) x y :=
-  by
-  refine'
-    JoinedIn.ofLine line_map_continuous.continuous_on (line_map_apply_zero _ _)
-      (line_map_apply_one _ _) _
+    (hy : y ∉ Submodule.span ℝ ({x} : Set F)) : JoinedIn ({0}ᶜ : Set F) x y := by
+  refine' JoinedIn.ofLine lineMap_continuous.continuousOn (lineMap_apply_zero _ _)
+      (lineMap_apply_one _ _) _
   rw [← segment_eq_image_lineMap]
   exact fun t ht (h' : t = 0) => (mt (mem_span_of_zero_mem_segment hx) hy) (h' ▸ ht)
 
 /-- In a vector space whose dimension is at least 2, the complement of
 `{0}` is ample. -/
 theorem isPathConnected_compl_zero_of_two_le_dim (hdim : 2 ≤ Module.rank ℝ F) :
-    IsPathConnected ({0}ᶜ : Set F) :=
-  by
+    IsPathConnected ({0}ᶜ : Set F) := by
   rw [isPathConnected_iff]
   constructor
   · suffices 0 < Module.rank ℝ F by rwa [rank_pos_iff_exists_ne_zero] at this 
     exact lt_of_lt_of_le (by norm_num) hdim
   · intro x hx y hy
     by_cases h : y ∈ Submodule.span ℝ ({x} : Set F)
-    · suffices ∃ z, z ∉ Submodule.span ℝ ({x} : Set F)
-        by
-        rcases this with ⟨z, hzx⟩
-        have hzy : z ∉ Submodule.span ℝ ({y} : Set F) := fun h' =>
+    · rsuffices ⟨z, hzx⟩ : ∃ z, z ∉ Submodule.span ℝ ({x} : Set F)
+      · have hzy : z ∉ Submodule.span ℝ ({y} : Set F) := fun h' =>
           hzx (Submodule.mem_span_singleton_trans h' h)
-        exact
-          (joinedIn_compl_zero_of_not_mem_span hx hzx).trans
-            (joinedIn_compl_zero_of_not_mem_span hy hzy).symm
+        exact (joinedIn_compl_zero_of_not_mem_span hx hzx).trans
+          (joinedIn_compl_zero_of_not_mem_span hy hzy).symm
       by_contra h'
       push_neg at h' 
       rw [← Submodule.eq_top_iff'] at h' 
@@ -202,34 +189,32 @@ theorem isPathConnected_compl_zero_of_two_le_dim (hdim : 2 ≤ Module.rank ℝ F
 /-- Let `E` be a linear subspace in a real vector space. If `E` has codimension at
 least two then its complement is path-connected. -/
 theorem isPathConnected_compl_of_two_le_codim {E : Submodule ℝ F}
-    (hcodim : 2 ≤ Module.rank ℝ (F ⧸ E)) : IsPathConnected (Eᶜ : Set F) :=
-  by
-  rcases E.exists_is_compl with ⟨E', hE'⟩
+    (hcodim : 2 ≤ Module.rank ℝ (F ⧸ E)) : IsPathConnected (Eᶜ : Set F) := by
+  rcases E.exists_isCompl with ⟨E', hE'⟩
   refine' isPathConnected_compl_of_isPathConnected_compl_zero hE'.symm _
   refine' isPathConnected_compl_zero_of_two_le_dim _
-  rwa [← (E.quotient_equiv_of_is_compl E' hE').rank_eq]
+  rwa [← (E.quotientEquivOfIsCompl E' hE').rank_eq]
 
 /-- Let `E` be a linear subspace in a real vector space. If `E` has codimension at
 least two then its complement is connected. -/
 theorem isConnected_compl_of_two_le_codim {E : Submodule ℝ F} (hcodim : 2 ≤ Module.rank ℝ (F ⧸ E)) :
     IsConnected (Eᶜ : Set F) :=
-  (isPathConnected_compl_of_two_le_codim hcodim).IsConnected
+  (isPathConnected_compl_of_two_le_codim hcodim).isConnected
 
 theorem Submodule.connectedComponentIn_eq_self_of_two_le_codim (E : Submodule ℝ F)
     (hcodim : 2 ≤ Module.rank ℝ (F ⧸ E)) {x : F} (hx : x ∉ E) :
-    connectedComponentIn ((E : Set F)ᶜ) x = Eᶜ :=
+    connectedComponentIn ((E : Set F)ᶜ) x = (E : Set F)ᶜ :=
   IsPreconnected.connectedComponentIn (isConnected_compl_of_two_le_codim hcodim).2 hx
 
 /-- Let `E` be a linear subspace in a real vector space. If `E` has codimension at
 least two then its complement is ample. -/
 theorem ample_of_two_le_codim {E : Submodule ℝ F} (hcodim : 2 ≤ Module.rank ℝ (F ⧸ E)) :
-    AmpleSet (Eᶜ : Set F) := by
-  intro x hx
-  rw [E.connected_component_in_eq_self_of_two_le_codim hcodim hx, eq_univ_iff_forall]
+    AmpleSet (Eᶜ : Set F) := fun x hx ↦ by
+  rw [E.connectedComponentIn_eq_self_of_two_le_codim hcodim hx, eq_univ_iff_forall]
   intro y
   by_cases h : y ∈ E
-  · rcases E.exists_is_compl with ⟨E', hE'⟩
-    rw [(E.quotient_equiv_of_is_compl E' hE').rank_eq] at hcodim 
+  · rcases E.exists_isCompl with ⟨E', hE'⟩
+    rw [(E.quotientEquivOfIsCompl E' hE').rank_eq] at hcodim 
     have hcodim' : 0 < Module.rank ℝ E' := lt_of_lt_of_le (by norm_num) hcodim
     rw [rank_pos_iff_exists_ne_zero] at hcodim' 
     rcases hcodim' with ⟨z, hz⟩
