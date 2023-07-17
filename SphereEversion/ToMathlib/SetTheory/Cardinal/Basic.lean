c75d69663db787fcdacfcedfafc06b1c790a6cad
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 01:43:25 -0500

    Fix

--- SphereEversion/ToMathlib/SetTheory/Cardinal/Basic.lean
+++ SphereEversion/ToMathlib/SetTheory/Cardinal/Basic.lean
@@ -5,8 +5,7 @@ open Cardinal
 open scoped Cardinal
 
 theorem Set.countable_infinite_iff_nonempty_denumerable {α : Type _} {s : Set α} :
-    s.Countable ∧ s.Infinite ↔ Nonempty (Denumerable s) :=
-  by
-  rw [denumerable_iff, ← Set.infinite_coe_iff, infinite_iff, ← le_aleph_0_iff_set_countable]
-  exact ⟨fun h => le_antisymm h.1 h.2, fun h => ⟨by rw [h], by rw [h]⟩⟩
+    s.Countable ∧ s.Infinite ↔ Nonempty (Denumerable s) := by
+  rw [denumerable_iff, ← Set.infinite_coe_iff, infinite_iff, ← le_aleph0_iff_set_countable,
+    le_antisymm_iff]
 
