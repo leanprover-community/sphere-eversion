5349405c7d53fdfad22d41fea13cd11341c8ea1b
Yury G. Kudryashov <urkud@urkud.name>
2023-07-18 00:10:55 -0500

    Fix a file

--- SphereEversion/ToMathlib/Geometry/Manifold/ChartedSpace.lean
+++ SphereEversion/ToMathlib/Geometry/Manifold/ChartedSpace.lean
@@ -14,14 +14,12 @@ theorem iUnion_source_eq_univ : (⋃ x : M, (chartAt H x).source) = (univ : Set 
 variable {M}
 
 theorem isOpen_iff (s : Set M) :
-    IsOpen s ↔ ∀ x : M, IsOpen <| chartAt H x '' ((chartAt H x).source ∩ s) :=
-  by
+    IsOpen s ↔ ∀ x : M, IsOpen <| chartAt H x '' ((chartAt H x).source ∩ s) := by
   refine' ⟨fun h x => (chartAt H x).image_open_of_open' h, fun h => _⟩
-  rw [← s.inter_univ, ← Union_source_eq_univ H M, s.inter_iUnion]
+  rw [← s.inter_univ, ← iUnion_source_eq_univ H M, s.inter_iUnion]
   refine' isOpen_iUnion fun x => _
   have : s ∩ (chartAt H x).source ⊆ (chartAt H x).source := inter_subset_right _ _
   rw [(chartAt H x).isOpen_image_iff_of_subset_source this, inter_comm]
   exact h x
 
 end ChartedSpace
-
