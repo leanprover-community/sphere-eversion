131a9e71b52418155a162c2a8c9ea7e9a0e24840
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 20:57:49 -0500

    Fix

--- SphereEversion/ToMathlib/Analysis/Calculus/AffineMap.lean
+++ SphereEversion/ToMathlib/Analysis/Calculus/AffineMap.lean
@@ -14,27 +14,25 @@ open Set Function Metric AffineMap
 variable {F : Type _} [NormedAddCommGroup F] [NormedSpace ℝ F]
 
 -- Unused
-@[simp]
-theorem range_affine_equiv_ball {p c : F} {s r : ℝ} (hr : 0 < r) :
-    (range fun x : ball p s => c +ᵥ homothety p r (x : F)) = ball (c + p) (r * s) :=
-  by
-  ext
-  simp only [homothety_apply, dist_eq_norm, vsub_eq_sub, vadd_eq_add, mem_range, SetCoe.exists,
-    mem_ball, Subtype.coe_mk, exists_prop]
-  refine' ⟨_, fun h => ⟨p + r⁻¹ • (x - (c + p)), _, _⟩⟩
-  · rintro ⟨y, h, rfl⟩
-    simpa [norm_smul, abs_eq_self.mpr hr.le] using (mul_lt_mul_left hr).mpr h
-  · simpa [norm_smul, abs_eq_self.mpr hr.le] using (inv_mul_lt_iff hr).mpr h
-  · simp [← smul_assoc, hr.ne.symm.is_unit.mul_inv_cancel]; abel
+-- @[simp]
+-- theorem range_affine_equiv_ball {p c : F} {s r : ℝ} (hr : 0 < r) :
+--     (range fun x : ball p s => c +ᵥ homothety p r (x : F)) = ball (c + p) (r * s) := by
+--   ext
+--   simp only [homothety_apply, dist_eq_norm, vsub_eq_sub, vadd_eq_add, mem_range, SetCoe.exists,
+--     mem_ball, Subtype.coe_mk, exists_prop]
+--   refine' ⟨_, fun h => ⟨p + r⁻¹ • (x - (c + p)), _, _⟩⟩
+--   · rintro ⟨y, h, rfl⟩
+--     simpa [norm_smul, abs_eq_self.mpr hr.le] using (mul_lt_mul_left hr).mpr h
+--   · simpa [norm_smul, abs_eq_self.mpr hr.le] using (inv_mul_lt_iff hr).mpr h
+--   · simp [← smul_assoc, hr.ne.symm.is_unit.mul_inv_cancel]; abel
 
 @[simp]
-theorem norm_coe_ball_lt (r : ℝ) (x : ball (0 : F) r) : ‖(x : F)‖ < r := by cases' x with x hx;
+theorem norm_coe_ball_lt (r : ℝ) (x : ball (0 : F) r) : ‖(x : F)‖ < r := by
+  cases' x with x hx
   simpa using hx
 
 theorem mapsTo_homothety_ball (c : F) {r : ℝ} (hr : 0 < r) :
-    MapsTo (fun y => homothety c r⁻¹ y -ᵥ c) (ball c r) (ball 0 1) :=
-  by
-  intro y hy
+    MapsTo (fun y => homothety c r⁻¹ y -ᵥ c) (ball c r) (ball 0 1) := fun y hy ↦ by
   replace hy : r⁻¹ * ‖y - c‖ < 1
   · rw [← mul_lt_mul_left hr, ← mul_assoc, mul_inv_cancel hr.ne.symm, mul_one, one_mul]
     simpa [dist_eq_norm] using hy
