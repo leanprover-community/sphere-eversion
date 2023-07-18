760e70573804576fdd736b781dade307d564afcd
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 21:04:11 -0500

    Fix a file

--- SphereEversion/ToMathlib/Topology/LocallyFinite.lean
+++ SphereEversion/ToMathlib/Topology/LocallyFinite.lean
@@ -3,20 +3,16 @@ import Mathlib.Topology.LocallyFinite
 open Function Set
 
 theorem LocallyFinite.smul_left {ι : Type _} {α : Type _} [TopologicalSpace α] {M : Type _}
-    {R : Type _} [Semiring R] [AddCommMonoid M] [Module R M] [NoZeroSMulDivisors R M]
+    {R : Type _} [Zero R] [Zero M] [SMulWithZero R M]
     {s : ι → α → R} (h : LocallyFinite fun i => support <| s i) (f : ι → α → M) :
-    LocallyFinite fun i => support <| s i • f i := by
-  apply h.subset fun i => _
-  rw [support_smul]
-  exact inter_subset_left _ _
+    LocallyFinite fun i => support (s i • f i) :=
+  h.subset fun i x ↦ mt <| fun h ↦ by rw [Pi.smul_apply', h, zero_smul]
 
 theorem LocallyFinite.smul_right {ι : Type _} {α : Type _} [TopologicalSpace α] {M : Type _}
-    {R : Type _} [Semiring R] [AddCommMonoid M] [Module R M] [NoZeroSMulDivisors R M]
+    {R : Type _} [Zero R] [Zero M] [SMulZeroClass R M]
     {f : ι → α → M} (h : LocallyFinite fun i => support <| f i) (s : ι → α → R) :
-    LocallyFinite fun i => support <| s i • f i := by
-  apply h.subset fun i => _
-  rw [support_smul]
-  exact inter_subset_right _ _
+    LocallyFinite fun i => support <| s i • f i :=
+  h.subset fun i x ↦ mt <| fun h ↦ by rw [Pi.smul_apply', h, smul_zero]
 
 section
 
@@ -25,10 +21,9 @@ variable {ι X : Type _} [TopologicalSpace X]
 @[to_additive]
 theorem LocallyFinite.exists_finset_mulSupport_eq {M : Type _} [CommMonoid M] {ρ : ι → X → M}
     (hρ : LocallyFinite fun i => mulSupport <| ρ i) (x₀ : X) :
-    ∃ I : Finset ι, (mulSupport fun i => ρ i x₀) = I :=
-  by
+    ∃ I : Finset ι, (mulSupport fun i => ρ i x₀) = I := by
   use (hρ.point_finite x₀).toFinset
-  rw [finite.coe_to_finset]
+  rw [Finite.coe_toFinset]
   rfl
 
 end
@@ -39,10 +34,7 @@ open Filter
 
 theorem LocallyFinite.eventually_subset {ι X : Type _} [TopologicalSpace X] {s : ι → Set X}
     (hs : LocallyFinite s) (hs' : ∀ i, IsClosed (s i)) (x : X) :
-    ∀ᶠ y in 𝓝 x, {i | y ∈ s i} ⊆ {i | x ∈ s i} :=
-  by
-  apply mem_of_superset (hs.Inter_compl_mem_nhds hs' x)
-  intro y hy i hi
-  simp only [mem_Inter, mem_compl_iff] at hy
+    ∀ᶠ y in 𝓝 x, {i | y ∈ s i} ⊆ {i | x ∈ s i} := by
+  filter_upwards [hs.iInter_compl_mem_nhds hs' x] with y hy i hi
+  simp only [mem_iInter, mem_compl_iff] at hy
   exact not_imp_not.mp (hy i) hi
-
