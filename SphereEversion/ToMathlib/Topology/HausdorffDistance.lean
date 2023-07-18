bf6911f065b387a680a1bbf9676272d9c4c4de70
Yury G. Kudryashov <urkud@urkud.name>
2023-07-18 00:25:49 -0500

    Fix +1 file

--- SphereEversion/ToMathlib/Topology/HausdorffDistance.lean
+++ SphereEversion/ToMathlib/Topology/HausdorffDistance.lean
@@ -8,18 +8,13 @@ variable {α β : Type _} [PseudoMetricSpace α] [PseudoMetricSpace β]
 
 namespace Metric
 
-theorem thickening_ball (x : α) (ε δ : ℝ) : thickening ε (ball x δ) ⊆ ball x (ε + δ) :=
-  by
-  intro y
-  simp only [mem_thickening_iff, mem_ball]
-  rintro ⟨z, hz, hz'⟩
-  calc
-    dist y x ≤ dist y z + dist z x := dist_triangle _ _ _
-    _ < ε + δ := add_lt_add hz' hz
+theorem thickening_ball (x : α) (ε δ : ℝ) : thickening ε (ball x δ) ⊆ ball x (ε + δ) := by
+  rw [← thickening_singleton, ← thickening_singleton]
+  apply thickening_thickening_subset
 
 theorem infDist_pos_iff_not_mem_closure {x : α} {s : Set α} (hs : s.Nonempty) :
     0 < infDist x s ↔ x ∉ closure s := by
-  rw [is_closed_closure.not_mem_iff_inf_dist_pos hs.closure, inf_dist_eq_closure]
+  rw [isClosed_closure.not_mem_iff_infDist_pos hs.closure, infDist_closure]
 
 end Metric
 
@@ -27,14 +22,10 @@ open Metric
 
 theorem IsCompact.exists_thickening_image {f : α → β} {K : Set α} {U : Set β} (hK : IsCompact K)
     (ho : IsOpen U) (hf : Continuous f) (hKU : MapsTo f K U) :
-    ∃ ε > 0, ∃ V ∈ 𝓝ˢ K, thickening ε (f '' V) ⊆ U :=
-  by
-  rcases(hK.image hf).exists_thickening_subset_open ho hKU.image_subset with ⟨r, hr₀, hr⟩
-  refine'
-    ⟨r / 2, half_pos hr₀, f ⁻¹' thickening (r / 2) (f '' K),
-      (isOpen_thickening.preimage hf).mem_nhdsSet.2 <|
-        image_subset_iff.mp <| self_subset_thickening (half_pos hr₀) _,
-      _⟩
+    ∃ ε > 0, ∃ V ∈ 𝓝ˢ K, thickening ε (f '' V) ⊆ U := by
+  rcases (hK.image hf).exists_thickening_subset_open ho hKU.image_subset with ⟨r, hr₀, hr⟩
+  refine ⟨r / 2, half_pos hr₀, f ⁻¹' thickening (r / 2) (f '' K),
+    hf.tendsto_nhdsSet (mapsTo_image _ _) (thickening_mem_nhdsSet _ (half_pos hr₀)), ?_⟩
   calc
     thickening (r / 2) (f '' (f ⁻¹' thickening (r / 2) (f '' K))) ⊆
         thickening (r / 2) (thickening (r / 2) (f '' K)) :=
@@ -42,4 +33,3 @@ theorem IsCompact.exists_thickening_image {f : α → β} {K : Set α} {U : Set 
     _ ⊆ thickening (r / 2 + r / 2) (f '' K) := (thickening_thickening_subset _ _ _)
     _ = thickening r (f '' K) := by rw [add_halves]
     _ ⊆ U := hr
-
