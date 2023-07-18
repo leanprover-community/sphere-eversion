0d62b0483e7aac5441fb2402d6f10889996a515c
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 21:11:49 -0500

    Fix a file

--- SphereEversion/ToMathlib/Topology/LocalHomeomorph.lean
+++ SphereEversion/ToMathlib/Topology/LocalHomeomorph.lean
@@ -5,12 +5,12 @@ variable {α β : Type _} [TopologicalSpace α] [TopologicalSpace β] (e : Local
 namespace LocalHomeomorph
 
 theorem isOpen_symm_image_iff_of_subset_target {s : Set β} (hs : s ⊆ e.target) :
-    IsOpen (e.symm '' s) ↔ IsOpen s :=
-  by
+    IsOpen (e.symm '' s) ↔ IsOpen s := by
   refine' ⟨fun h => _, fun h => e.symm.image_open_of_open h hs⟩
-  have hs' : e.symm '' s ⊆ e.source := by rw [e.symm_image_eq_source_inter_preimage hs];
+  have hs' : e.symm '' s ⊆ e.source := by
+    rw [e.symm_image_eq_source_inter_preimage hs]
     apply Set.inter_subset_left
-  rw [← e.to_local_equiv.image_symm_image_of_subset_target hs]
+  rw [← e.image_symm_image_of_subset_target hs]
   exact e.image_open_of_open h hs'
 
 theorem isOpen_image_iff_of_subset_source {s : Set α} (hs : s ⊆ e.source) :
