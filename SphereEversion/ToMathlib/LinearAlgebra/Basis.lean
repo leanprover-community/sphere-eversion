2eb38d5901b099895cccf0aed9ff387db55d479a
Yury G. Kudryashov <urkud@urkud.name>
2023-07-17 22:09:52 -0500

    Fix

--- SphereEversion/ToMathlib/LinearAlgebra/Basis.lean
+++ SphereEversion/ToMathlib/LinearAlgebra/Basis.lean
@@ -43,29 +43,22 @@ theorem Basis.flag_mono {n : ℕ} (b : Basis (Fin n) R M) : Monotone b.flag := f
   exact hl.trans_le h
 
 theorem Fin.coe_succ_le_iff_le {n : ℕ} {j k : Fin n} : (j : Fin <| n + 1) ≤ k ↔ j ≤ k := by
-  cases j
-  cases k
-  simp [*]
+  simp only [Fin.coe_eq_castSucc]; rfl
 
 theorem Fin.coe_succ_lt_iff_lt {n : ℕ} {j k : Fin n} : (j : Fin <| n + 1) < k ↔ j < k := by
-  cases j
-  cases k
-  simp
+  simp only [Fin.coe_eq_castSucc]; rfl
 
-theorem Fin.coe_lt_succ {n : ℕ} (k : Fin n) : (k : Fin <| n + 1) < k.succ :=
-  by
-  cases k
-  simp
+theorem Fin.coe_lt_succ {n : ℕ} (k : Fin n) : (k : Fin <| n + 1) < k.succ := by
+  rw [Fin.coe_eq_castSucc]
+  exact Nat.lt_succ_self _
 
 theorem Basis.flag_span_succ {n : ℕ} (b : Basis (Fin n) R M) (k : Fin n) :
     b.flag k ⊔ span R {b k} = b.flag k.succ := by
-  rw [Basis.flag, ← span_union, ← image_singleton, ← image_union]
-  congr
-  ext j
+  rw [Basis.flag, ← span_union, ← image_singleton, ← image_union, flag]
+  refine congr_arg (span R <| b '' ·) <| Set.ext fun j ↦ ?_
   have : j = k ∨ j < k ↔ ↑j < k.succ := by
-    cases j
-    cases k
-    simp [← le_iff_eq_or_lt, nat.lt_succ_iff.symm]
+    rw [← le_iff_eq_or_lt, Fin.coe_eq_castSucc, Fin.lt_iff_val_lt_val]
+    exact Nat.lt_succ_iff.symm
   simp [this]
 
 end
