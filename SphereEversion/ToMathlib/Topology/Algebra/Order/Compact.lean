05cff6a1d7a740077c4b090d2eb058da4d239df1
Yury G. Kudryashov <urkud@urkud.name>
2023-07-19 10:17:08 -0500

    Fix, golf

--- SphereEversion/ToMathlib/Topology/Algebra/Order/Compact.lean
+++ SphereEversion/ToMathlib/Topology/Algebra/Order/Compact.lean
@@ -5,10 +5,8 @@ import Mathlib.Topology.Instances.Real
 assumption `s.nonempty`. -/
 theorem IsCompact.exists_forall_le' {β : Type _} [TopologicalSpace β] {s : Set β} (hs : IsCompact s)
     {f : β → ℝ} (hf : ContinuousOn f s) {a : ℝ} (hf' : ∀ b ∈ s, a < f b) :
-    ∃ a', a < a' ∧ ∀ b ∈ s, a' ≤ f b :=
-  by
+    ∃ a', a < a' ∧ ∀ b ∈ s, a' ≤ f b := by
   rcases s.eq_empty_or_nonempty with (rfl | hs')
-  · exact ⟨a + 1, by simp only [lt_add_iff_pos_right, zero_lt_one], fun b hb => by simpa using hb⟩
+  · exact ⟨a + 1, by simp⟩
   · obtain ⟨x, hx, hx'⟩ := hs.exists_forall_le hs' hf
     exact ⟨f x, hf' x hx, hx'⟩
-
