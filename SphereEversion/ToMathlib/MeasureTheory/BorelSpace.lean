c95101e200a450ba3b37e2e9f937fd83dfeee96a
Yury G. Kudryashov <urkud@urkud.name>
2023-07-18 19:49:51 -0500

    Fix a file

--- SphereEversion/ToMathlib/MeasureTheory/BorelSpace.lean
+++ SphereEversion/ToMathlib/MeasureTheory/BorelSpace.lean
@@ -5,7 +5,7 @@ variable (α : Type _) [TopologicalSpace α]
 scoped[Borelize] attribute [instance] borel
 
 theorem borelSpace_borel : @BorelSpace α _ (borel α) :=
-  ⟨rfl⟩
+  letI := borel α; ⟨rfl⟩
 
 scoped[Borelize] attribute [instance] borelSpace_borel
 
