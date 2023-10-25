c2f670c3d3c769e897066348ccce34b71194323b
Patrick Massot <patrickmassot@free.fr>
2023-10-25 14:18:53 -0400

    TwistedOneJetSec modulo sorries

--- SphereEversion/Global/TwistOneJetSec.lean
+++ SphereEversion/Global/TwistOneJetSec.lean
@@ -26,21 +26,17 @@ variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type u} [NormedAddCo
 
 section Smoothness
 
-/- set_option hygiene false
-local notation "FJ¹MV" =>
-  Bundle.ContinuousLinearMap (RingHom.id 𝕜) (TangentSpace I : M → Type u) (Bundle.Trivial M V)
-set_option hygiene true
 
-local notation "J¹MV" => TotalSpace (E →L[𝕜] V) FJ¹MV -/
-
+notation "J¹[" 𝕜 ", " E ", " I ", " M ", " V "]" => TotalSpace (E →L[𝕜] V)
+  (Bundle.ContinuousLinearMap (RingHom.id 𝕜) (TangentSpace I : M → _) (Bundle.Trivial M V))
 variable {I M V}
-variable {f : N → TotalSpace (E →L[𝕜] V) (Bundle.ContinuousLinearMap (RingHom.id 𝕜) (TangentSpace I : M → Type u) (Bundle.Trivial M V))}
+variable {f : N → J¹[𝕜, E, I, M, V]}
 
 -- todo: remove or use to prove `smooth_at_one_jet_eucl_bundle`
 theorem smoothAt_one_jet_eucl_bundle' {x₀ : N} :
-    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V)) f x₀ ↔ SmoothAt J I (fun x => (f x).1) x₀ ∧
-    SmoothAt J 𝓘(𝕜, E →L[𝕜] V)
-      (fun x => show E →L[𝕜] V from (f x).2 ∘L (trivializationAt E (TangentSpace I : M → Type u) (f x₀).1).symmL 𝕜 (f x).1) x₀ := by
+    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V)) f x₀ ↔ SmoothAt J I (fun x  ↦ (f x).1) x₀ ∧
+    SmoothAt J 𝓘(𝕜, E →L[𝕜] V) (fun x  ↦ show E →L[𝕜] V from
+      (f x).2 ∘L (trivializationAt E (TangentSpace I : M → _) (f x₀).1).symmL 𝕜 (f x).1) x₀ := by
   simp_rw [smoothAt_hom_bundle, inCoordinates, Trivial.trivializationAt,
     Trivial.trivialization_continuousLinearMapAt]
   dsimp only [Bundle.Trivial]
@@ -48,12 +44,9 @@ theorem smoothAt_one_jet_eucl_bundle' {x₀ : N} :
 
 theorem smoothAt_one_jet_eucl_bundle {x₀ : N} :
     SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V)) f x₀ ↔
-      SmoothAt J I (fun x => (f x).1) x₀ ∧
-        SmoothAt J 𝓘(𝕜, E →L[𝕜] V)
-          (fun x =>
-            show E →L[𝕜] V from
-              (f x).2 ∘L (trivializationAt E (TangentSpace I) (f x₀).proj).symmL 𝕜 (f x).proj)
-          x₀ := by
+      SmoothAt J I (fun x  ↦ (f x).1) x₀ ∧
+        SmoothAt J 𝓘(𝕜, E →L[𝕜] V) (fun x  ↦ show E →L[𝕜] V from
+          (f x).2 ∘L (trivializationAt E (TangentSpace I) (f x₀).proj).symmL 𝕜 (f x).proj) x₀ := by
   rw [smoothAt_hom_bundle, and_congr_right_iff]
   intro hf
   refine' Filter.EventuallyEq.contMDiffAt_iff _
@@ -69,23 +62,18 @@ theorem smoothAt_one_jet_eucl_bundle {x₀ : N} :
 
 theorem SmoothAt.one_jet_eucl_bundle_mk' {f : N → M} {ϕ : N → E →L[𝕜] V} {x₀ : N}
     (hf : SmoothAt J I f x₀)
-    (hϕ :
-      SmoothAt J 𝓘(𝕜, E →L[𝕜] V)
-        (fun x =>
-          show E →L[𝕜] V from
-            ϕ x ∘L (trivializationAt E (TangentSpace I : M → Type*) (f x₀)).symmL 𝕜 (f x))
-        x₀) :
-    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V)) (fun x => Bundle.TotalSpace.mk (f x) (ϕ x) : N → J¹MV) x₀ :=
+    (hϕ : SmoothAt J 𝓘(𝕜, E →L[𝕜] V) (fun x ↦ show E →L[𝕜] V from
+            ϕ x ∘L (trivializationAt E (TangentSpace I : M → _) (f x₀)).symmL 𝕜 (f x)) x₀) :
+    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V))
+      (fun x ↦ Bundle.TotalSpace.mk (f x) (ϕ x) : N → J¹[𝕜, E, I, M, V]) x₀ :=
   smoothAt_one_jet_eucl_bundle'.mpr ⟨hf, hϕ⟩
 
 theorem SmoothAt.one_jet_eucl_bundle_mk {f : N → M} {ϕ : N → E →L[𝕜] V} {x₀ : N}
     (hf : SmoothAt J I f x₀)
-    (hϕ :
-      SmoothAt J 𝓘(𝕜, E →L[𝕜] V)
-        (fun x =>
-          show E →L[𝕜] V from ϕ x ∘L (trivializationAt E (TangentSpace I) (f x₀)).symmL 𝕜 (f x))
-        x₀) :
-    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V)) (fun x => Bundle.TotalSpace.mk (f x) (ϕ x) : N → J¹MV) x₀ :=
+    (hϕ : SmoothAt J 𝓘(𝕜, E →L[𝕜] V) (fun x ↦ show E →L[𝕜] V from
+      ϕ x ∘L (trivializationAt E (TangentSpace I) (f x₀)).symmL 𝕜 (f x)) x₀) :
+    SmoothAt J (I.prod 𝓘(𝕜, E →L[𝕜] V))
+      (fun x ↦ Bundle.TotalSpace.mk (f x) (ϕ x) : N → J¹[𝕜, E, I, M, V]) x₀ :=
   smoothAt_one_jet_eucl_bundle.mpr ⟨hf, hϕ⟩
 
 end Smoothness
@@ -95,14 +83,14 @@ section Sections
 /-- A section of a 1-jet bundle seen as a bundle over the source manifold. -/
 @[ext]
 structure OneJetEuclSec where
-  toFun : M → J¹MV
-  is_sec' : ∀ p, (to_fun p).1 = p
-  smooth' : Smooth I (I.prod 𝓘(𝕜, E →L[𝕜] V)) to_fun
+  toFun : M → J¹[𝕜, E, I, M, V]
+  is_sec' : ∀ p, (toFun p).1 = p
+  smooth' : Smooth I (I.prod 𝓘(𝕜, E →L[𝕜] V)) toFun
 
 variable {I M V}
 
-instance : CoeFun (OneJetEuclSec I M V) fun S => M → J¹MV :=
-  ⟨fun S x => S.toFun x⟩
+instance : CoeFun (OneJetEuclSec I M V) fun _  ↦ M → J¹[𝕜, E, I, M, V] :=
+  ⟨fun S x  ↦ S.toFun x⟩
 
 @[simp]
 theorem OneJetEuclSec.is_sec (s : OneJetEuclSec I M V) (p : M) : (s p).1 = p :=
@@ -116,8 +104,8 @@ end Sections
 
 section proj
 
-instance piBugInstanceRestatement (x : M) :
-    TopologicalSpace (Bundle.ContinuousLinearMap (RingHom.id 𝕜) (TangentSpace I) (Trivial M V) x) := by
+instance piBugInstanceRestatement (x : M) : TopologicalSpace
+    (Bundle.ContinuousLinearMap (RingHom.id 𝕜) (TangentSpace I) (Trivial M V) x) := by
   infer_instance
 
 instance piBugInstanceRestatement2 (x : M × V) : TopologicalSpace (OneJetSpace I 𝓘(𝕜, V) x) := by
@@ -126,24 +114,23 @@ instance piBugInstanceRestatement2 (x : M × V) : TopologicalSpace (OneJetSpace 
 /- Given a smooth manifold `M` and a normed space `V`, there is a canonical projection from the
 one-jet bundle of maps from `M` to `V` to the bundle of homomorphisms from `TM` to `V`. This is
 constructed using the fact that each tangent space to `V` is canonically isomorphic to `V`. -/
-def proj (v : OneJetBundle I M 𝓘(𝕜, V) V) : J¹MV :=
+def proj (v : OneJetBundle I M 𝓘(𝕜, V) V) : J¹[𝕜, E, I, M, V] :=
   ⟨v.1.1, v.2⟩
 
-theorem smooth_proj :
-    Smooth ((I.prod 𝓘(𝕜, V)).prod 𝓘(𝕜, E →L[𝕜] V)) (I.prod 𝓘(𝕜, E →L[𝕜] V)) (proj I M V) :=
-  by
+theorem smooth_proj : Smooth ((I.prod 𝓘(𝕜, V)).prod 𝓘(𝕜, E →L[𝕜] V)) (I.prod 𝓘(𝕜, E →L[𝕜] V))
+    (proj I M V) := by
   intro x₀
   have : SmoothAt ((I.prod 𝓘(𝕜, V)).prod 𝓘(𝕜, E →L[𝕜] V)) _ id x₀ := smoothAt_id
-  simp_rw [smoothAt_oneJetBundle, inTangentCoordinates, in_coordinates, tangentBundleCore_indexAt,
+  sorry
+  /- simp_rw [smoothAt_oneJetBundle, inTangentCoordinates, inCoordinates, tangentBundleCore_indexAt,
     TangentBundle.continuousLinearMapAt_model_space, ContinuousLinearMap.one_def] at this
   dsimp only [TangentSpace] at this
   simp_rw [ContinuousLinearMap.id_comp] at this
-  refine' this.1.one_jet_eucl_bundle_mk this.2.2
+  refine' this.1.one_jet_eucl_bundle_mk this.2.2 -/
 
 variable {I M V}
 
-def drop (s : OneJetSec I M 𝓘(𝕜, V) V) : OneJetEuclSec I M V
-    where
+def drop (s : OneJetSec I M 𝓘(𝕜, V) V) : OneJetEuclSec I M V where
   toFun := (proj I M V).comp s
   is_sec' p := rfl
   smooth' := (smooth_proj I M V).comp s.smooth
@@ -156,28 +143,26 @@ section incl
 the product with V of the bundle of homomorphisms from `TM` to `V` to the one-jet bundle of maps
 from `M` to `V`. In fact this map is a diffeomorphism.  This is constructed using the fact that each
 tangent space to `V` is canonically isomorphic to `V`. -/
-def incl (v : J¹MV × V) : OneJetBundle I M 𝓘(𝕜, V) V :=
+def incl (v : J¹[𝕜, E, I, M, V] × V) : OneJetBundle I M 𝓘(𝕜, V) V :=
   ⟨(v.1.1, v.2), v.1.2⟩
 
-theorem smooth_incl :
-    Smooth ((I.prod 𝓘(𝕜, E →L[𝕜] V)).prod 𝓘(𝕜, V)) ((I.prod 𝓘(𝕜, V)).prod 𝓘(𝕜, E →L[𝕜] V))
-      (incl I M V) :=
-  by
+theorem smooth_incl : Smooth ((I.prod 𝓘(𝕜, E →L[𝕜] V)).prod 𝓘(𝕜, V))
+    ((I.prod 𝓘(𝕜, V)).prod 𝓘(𝕜, E →L[𝕜] V)) (incl I M V) := by
   intro x₀
   have : SmoothAt ((I.prod 𝓘(𝕜, E →L[𝕜] V)).prod 𝓘(𝕜, V)) _ Prod.fst x₀ := smoothAt_fst
   rw [smoothAt_one_jet_eucl_bundle] at this
   refine' this.1.oneJetBundle_mk smoothAt_snd _
-  dsimp only [inTangentCoordinates, in_coordinates, TangentSpace]
+  dsimp only [inTangentCoordinates, inCoordinates, TangentSpace]
   simp_rw [TangentBundle.continuousLinearMapAt_model_space, ContinuousLinearMap.one_def,
     ContinuousLinearMap.id_comp]
   exact this.2
 
 @[simp]
-theorem incl_fst_fst (v : J¹MV × V) : (incl I M V v).1.1 = v.1.1 :=
+theorem incl_fst_fst (v : J¹[𝕜, E, I, M, V] × V) : (incl I M V v).1.1 = v.1.1 :=
   rfl
 
 @[simp]
-theorem incl_snd (v : J¹MV × V) : (incl I M V v).1.2 = v.2 :=
+theorem incl_snd (v : J¹[𝕜, E, I, M, V] × V) : (incl I M V v).1.2 = v.2 :=
   rfl
 
 end incl
@@ -193,22 +178,15 @@ variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] {H : Type*} [Top
   [NormedSpace ℝ F] {G : Type*} [TopologicalSpace G] (J : ModelWithCorners ℝ F G) (N : Type*)
   [TopologicalSpace N] [ChartedSpace G N] [SmoothManifoldWithCorners J N]
 
-/- local notation "σ" => RingHom.id ℝ
-
-local notation "FJ¹MV" =>
-  Bundle.ContinuousLinearMap σ (TangentSpace I : M → Type*) (Bundle.Trivial M V)
-
-local notation "J¹MV" => TotalSpace (E →L[ℝ] V) FJ¹MV -/
-
 /-- A section of a 1-jet bundle seen as a bundle over the source manifold. -/
 @[ext]
 structure FamilyOneJetEuclSec where
-  toFun : N × M → J¹MV
-  is_sec' : ∀ p, (to_fun p).1 = p.2
-  smooth' : Smooth (J.prod I) (I.prod 𝓘(ℝ, E →L[ℝ] V)) to_fun
+  toFun : N × M → J¹[ℝ, E, I, M, V]
+  is_sec' : ∀ p, (toFun p).1 = p.2
+  smooth' : Smooth (J.prod I) (I.prod 𝓘(ℝ, E →L[ℝ] V)) toFun
 
-instance : CoeFun (FamilyOneJetEuclSec I M V J N) fun S => N × M → J¹MV :=
-  ⟨fun S x => S.toFun x⟩
+instance : CoeFun (FamilyOneJetEuclSec I M V J N) fun S  ↦ N × M → J¹[ℝ, E, I, M, V] :=
+  ⟨fun S x  ↦ S.toFun x⟩
 
 variable {I M V J N}
 
@@ -222,7 +200,7 @@ theorem FamilyOneJetEuclSec.smooth (s : FamilyOneJetEuclSec I M V J N) :
     Smooth (J.prod I) (I.prod 𝓘(ℝ, E →L[ℝ] V)) s :=
   s.smooth'
 
-variable {I M V J N V'}
+variable {V'}
 
 def familyJoin {f : N × M → V} (hf : Smooth (J.prod I) 𝓘(ℝ, V) f)
     (s : FamilyOneJetEuclSec I M V J N) : FamilyOneJetSec I M 𝓘(ℝ, V) V J N
@@ -231,12 +209,13 @@ def familyJoin {f : N × M → V} (hf : Smooth (J.prod I) 𝓘(ℝ, V) f)
   ϕ n m := (incl I M V (s (n, m), f (n, m))).2
   smooth' := by
     convert (smooth_incl I M V).comp (s.smooth.prod_mk hf)
-    ext p
+    sorry
+    /- ext p
     · simp
     · simp
     have : (p.1, p.2) = p := Prod.ext rfl rfl
     rw [this]
-    simp
+    simp -/
 
 def familyTwist (s : OneJetEuclSec I M V) (i : N × M → V →L[ℝ] V')
     (i_smooth : ∀ x₀ : N × M, SmoothAt (J.prod I) 𝓘(ℝ, V →L[ℝ] V') i x₀) :
@@ -246,13 +225,14 @@ def familyTwist (s : OneJetEuclSec I M V) (i : N × M → V →L[ℝ] V')
   is_sec' p := rfl
   smooth' := by
     intro x₀
-    refine' smooth_at_snd.one_jet_eucl_bundle_mk' _
+    refine' smoothAt_snd.one_jet_eucl_bundle_mk' _
     simp_rw [ContinuousLinearMap.comp_assoc]
-    have : SmoothAt (J.prod I) _ (fun x : N × M => _) x₀ := s.smooth.comp smooth_snd x₀
-    simp_rw [smoothAt_one_jet_eucl_bundle', s.is_sec] at this
+    have : SmoothAt (J.prod I) _ (fun x : N × M  ↦ _) x₀ := s.smooth.comp smooth_snd x₀
+    sorry
+    /- simp_rw [smoothAt_one_jet_eucl_bundle', s.is_sec] at this
     refine' (i_smooth x₀).clm_comp _
     convert this.2
     ext z
-    rw [s.is_sec]
+    rw [s.is_sec] -/
 
 end familyTwist
