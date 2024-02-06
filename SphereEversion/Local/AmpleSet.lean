5487c1421a7894567e0b211f505f4b6c97edec73
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-02-06 11:34:57 +0100

    Fix comment typo from the port; ought to regenerate the blueprint.

--- SphereEversion/Local/AmpleSet.lean
+++ SphereEversion/Local/AmpleSet.lean
@@ -67,7 +67,7 @@ theorem AmpleSet.preimage {s : Set F} (h : AmpleSet s) (L : E ≃L[ℝ] F) : Amp
 open scoped Pointwise
 
 /-- Translating a ample set is ample.
-We basically mimic `ample_set.image`. We could prove the common generalization using
+We basically mimic `AmpleSet.image`. We could prove the common generalization using
 continuous affine equivalences -/
 theorem AmpleSet.vadd [ContinuousAdd E] {s : Set E} (h : AmpleSet s) {y : E} :
     AmpleSet (y +ᵥ s) := by
