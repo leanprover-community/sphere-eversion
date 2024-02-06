ec05f319528e0e5c1d36f5a5c7b2dcececf11cd8
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-02-06 15:11:08 +0100

    Fix blueprint generation: by Ruben Van de Velde.

--- tasks.py
+++ tasks.py
@@ -3,8 +3,6 @@ import shutil
 from pathlib import Path
 from invoke import run, task
 
-from mathlibtools.lib import LeanProject
-
 from blueprint.tasks import web, bp, print, serve
 
 ROOT = Path(__file__).parent
@@ -17,12 +15,7 @@ def doc(ctx):
         run(f'pandoc -t html --mathjax -f markdown+tex_math_dollars+raw_tex {path.name} --template template.html -o ../docs/{path.with_suffix(".html").name}')
     os.chdir(cwd)
 
-@task
-def decls(ctx):
-    proj = LeanProject.from_path(ROOT)
-    proj.pickle_decls(ROOT/'decls.pickle')
-
-@task(doc, decls, bp, web)
+@task(doc, bp, web)
 def all(ctx):
     shutil.rmtree(ROOT/'docs'/'blueprint', ignore_errors=True)
     shutil.copytree(ROOT/'blueprint'/'web', ROOT/'docs'/'blueprint')
