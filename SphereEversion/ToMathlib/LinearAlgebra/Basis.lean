86b93232976b83ee5e89f1c3a7278441bb211089
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-03-09 20:26:11 +0100

    refactor(LinearAlgebra/Basis): revamp the file; mostly in mathlib already
    - The definition and first lemmas are in mathlib already (with a slight tweak).
    - Another proof needs fixing, as the mathlib definition is different. Fix it.
    - Two lemmas are not directly relevant; the others have been PRed.
    - Minimize imports and opens; use Type*

--- SphereEversion/ToMathlib/LinearAlgebra/Basis.lean
+++ SphereEversion/ToMathlib/LinearAlgebra/Basis.lean
@@ -1,57 +1,28 @@
-import Mathlib.LinearAlgebra.Basis
+import Mathlib.LinearAlgebra.Basis.Flag
 import Mathlib.LinearAlgebra.Dual
-import Mathlib.Tactic.Monotonicity
 
 noncomputable section
 
-universe u
-
-open Function Set Submodule
-
-open scoped Classical BigOperators
+open Set Submodule
 
 section
 
-variable {R : Type _} [Semiring R] {M : Type _} [AddCommMonoid M] [Module R M]
+variable {R : Type*} [Semiring R] {M : Type*} [AddCommMonoid M] [Module R M]
 
-/-- The span of the first `n` elements of an ordered basis. -/
-def Basis.flag {n : ℕ} (b : Basis (Fin n) R M) : Fin (n + 1) → Submodule R M := fun k ↦
-  span R (b '' {j | (j : Fin (n + 1)) < k})
-
-@[simp]
-theorem Basis.flag_zero {n : ℕ} (b : Basis (Fin n) R M) : b.flag 0 = ⊥ := by
-  simp only [Basis.flag, Fin.coe_eq_castSucc]
-  suffices {j : Fin n | Fin.castSuccEmb j < 0} = ∅ by simp [this]
-  ext l
-  simp [l.castSucc.zero_le]
-
-@[simp]
-theorem Basis.flag_last {n : ℕ} (b : Basis (Fin n) R M) : b.flag (Fin.last n) = ⊤ := by
-  have : {j : Fin n | (j : Fin <| n + 1) < Fin.last n} = univ := by
-    ext l
-    simp [Fin.castSucc_lt_last l]
-  simp_rw [Basis.flag, this]
-  simp [b.span_eq]
-
-attribute [mono] Submodule.span_mono
-
-@[simp]
-theorem Basis.flag_mono {n : ℕ} (b : Basis (Fin n) R M) : Monotone b.flag := fun j k h ↦ by
-  dsimp [Basis.flag]
-  mono*
-  rintro l (hl : ↑↑l < j)
-  exact hl.trans_le h
-
+-- not directly used
 theorem Fin.coe_succ_le_iff_le {n : ℕ} {j k : Fin n} : (j : Fin <| n + 1) ≤ k ↔ j ≤ k := by
   simp only [Fin.coe_eq_castSucc]; rfl
 
+-- PRed in #11265
 theorem Fin.coe_succ_lt_iff_lt {n : ℕ} {j k : Fin n} : (j : Fin <| n + 1) < k ↔ j < k := by
   simp only [Fin.coe_eq_castSucc]; rfl
 
+-- not directly used
 theorem Fin.coe_lt_succ {n : ℕ} (k : Fin n) : (k : Fin <| n + 1) < k.succ := by
   rw [Fin.coe_eq_castSucc]
   exact Nat.lt_succ_self _
 
+-- PRed to mathlib in #11264
 theorem Basis.flag_span_succ {n : ℕ} (b : Basis (Fin n) R M) (k : Fin n) :
     b.flag k ⊔ span R {b k} = b.flag k.succ := by
   rw [Basis.flag, ← span_union, ← image_singleton, ← image_union, flag]
@@ -65,13 +36,18 @@ end
 
 section
 
-variable {R : Type _} [CommRing R] {M : Type _} [AddCommGroup M] [Module R M]
+variable {R : Type*} [CommRing R] {M : Type*} [AddCommGroup M] [Module R M]
 
-variable {n : ℕ} (b : Basis (Fin n) R M)
+variable {n : ℕ}
 
-theorem Basis.flag_le_ker_dual (k : Fin n) : b.flag k ≤ LinearMap.ker (b.dualBasis k) := by
+-- PRed in #11265
+theorem Basis.flag_le_ker_dual (b : Basis (Fin n) R M) (k : Fin n) :
+    b.flag k ≤ LinearMap.ker (b.dualBasis k) := by
   erw [span_le]
-  rintro _ ⟨j, hj : (j : Fin <| n + 1) < k, rfl⟩
-  simp [(Fin.coe_succ_lt_iff_lt.mp hj).ne]
+  rintro _ ⟨j, hj : j.castSucc < k, rfl⟩
+  have : j < k := by rw [← Fin.coe_eq_castSucc, Fin.coe_succ_lt_iff_lt] at hj; exact hj
+  simp only [coe_dualBasis, SetLike.mem_coe, LinearMap.mem_ker, coord_apply, repr_self]
+  rw [Finsupp.single_apply_eq_zero]
+  exact fun h ↦ False.elim (by omega)
 
 end
