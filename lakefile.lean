34a180a648f1b4d25f052d2d376ddacaef185b6c
Patrick Massot <patrickmassot@free.fr>
2024-05-23 16:16:22 -0400

    Try to modernize blueprint setup

--- lakefile.lean
+++ lakefile.lean
@@ -12,3 +12,9 @@ require mathlib from git
 
 @[default_target]
 lean_lib «SphereEversion» where
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
