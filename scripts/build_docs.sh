9ad62a88555f0979cdcdcfec4b655763cfb6d61f
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-03-05 14:07:16 +0100

    chore: upgrade doc-gen workflow (#103)
    * chore: upgrade doc-gen workflow
    
    * delete docbuild

---
+++ scripts/build_docs.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status,
+# treat unset variables as an error, and ensure errors in pipelines are not masked.
+set -euo pipefail
+
+# Build HTML documentation for SphereEversion
+# The output will be located in docs/docs
+
+# Template lakefile.toml
+template() {
+  cat <<EOF
+name = "docbuild"
+reservoir = false
+version = "0.1.0"
+packagesDir = "../.lake/packages"
+
+[[require]]
+name = "SphereEversion"
+path = "../"
+
+[[require]]
+scope = "leanprover"
+name = "doc-gen4"
+rev = "TOOLCHAIN"
+EOF
+}
+
+# Create a temporary docbuild folder
+mkdir -p docbuild
+
+# Equip docbuild with the template lakefile.toml
+template > docbuild/lakefile.toml
+
+# Substitute the toolchain from lean-toolchain into docbuild/lakefile.toml
+sed -i s/TOOLCHAIN/`grep -oP 'v4\..*' lean-toolchain`/ docbuild/lakefile.toml
+
+# Initialise docbuild as a Lean project
+cd docbuild
+
+# Disable an error message due to a non-blocking bug. See Zulip
+MATHLIB_NO_CACHE_ON_UPDATE=1 ~/.elan/bin/lake update SphereEversion
+
+# Build the docs
+~/.elan/bin/lake build SphereEversion:docs
+
+# Copy documentation to `docs/docs`
+cd ../
+sudo chown -R runner docs
+cp -r docbuild/.lake/build/doc docs/docs
