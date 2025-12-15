3d6b76c0ed66db74da0362479633274b608934f5
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-12-15 20:47:21 +0100

    chore: rephrase t2Space_iff_of_continuous_surjective_open

--- SphereEversion/ToMathlib/Topology/Separation/Hausdorff.lean
+++ SphereEversion/ToMathlib/Topology/Separation/Hausdorff.lean
@@ -1,18 +1,12 @@
 import Mathlib.Topology.Separation.Hausdorff
-open Set Function
 
-variable {X Y : Type*} [TopologicalSpace X] [TopologicalSpace Y]
-
-/-
-TODO? State a specialized version for quotient maps? Note the open map assumption is still
-a separate assumption there, because there is no `QuotientMap.prod_map`.
--/
-theorem t2Space_iff_of_continuous_surjective_open {α β : Type*} [TopologicalSpace α]
-    [TopologicalSpace β] {π : α → β} (hcont : Continuous π) (hsurj : Surjective π)
-    (hop : IsOpenMap π) : T2Space β ↔ IsClosed {q : α × α | π q.1 = π q.2} := by
+theorem t2Space_iff_of_isOpenQuotientMap {α β : Type*} [TopologicalSpace α]
+    [TopologicalSpace β] {π : α → β} (h : IsOpenQuotientMap π) :
+    T2Space β ↔ IsClosed {q : α × α | π q.1 = π q.2} := by
   rw [t2_iff_isClosed_diagonal]
+  replace h := IsOpenQuotientMap.prodMap h h
   constructor <;> intro H
-  · exact H.preimage (hcont.prodMap hcont)
+  · exact H.preimage h.continuous
   · simp_rw [← isOpen_compl_iff] at H ⊢
-    convert hop.prodMap hop _ H
-    exact ((hsurj.prodMap hsurj).image_preimage _).symm
+    convert h.isOpenMap _ H
+    exact (h.surjective.image_preimage _).symm
